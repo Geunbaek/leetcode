c0146a5ed457c2b4ebe35c7a287d71fcbb8ec1ac
Baek <74183179+Geunbaek@users.noreply.github.com>
2022-09-15 10:28:01 +0900

    Time: 852 ms (10.94%), Space: 66.1 MB (96.88%) - LeetHub

---
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.js
@@ -0,0 +1,41 @@
+/**
+ * @param {number[]} changed
+ * @return {number[]}
+ */
+const getCounter = (array) => {
+    const counter = new Map();
+    array.forEach(num => {
+        if(!counter[num]){
+            counter[num] = 0;
+        }
+        counter[num] += 1;
+    })
+    return counter;
+} 
+
+var findOriginalArray = function(changed) {
+    let totalCount = changed.length;
+    if(totalCount % 2 !== 0) return []
+    changed.sort((a, b) => a - b);
+    
+    const counter = getCounter(changed);
+    const answer = [];
+    
+    for(let i = 0; i <= changed.length; i ++){
+        const num = changed[i];
+        if(num === 0) {
+            if(counter[num] > 1){
+                counter[num] -= 2;
+                answer.push(num)
+                totalCount -= 2;
+            }
+        } else if(counter[num] > 0 && counter[num * 2] && counter[num * 2] > 0){
+            counter[num] -= 1;
+            counter[num * 2] -= 1;
+            answer.push(num);
+            totalCount -= 2;
+        } 
+    }
+    
+    return totalCount === 0 ? answer : [];
+};
