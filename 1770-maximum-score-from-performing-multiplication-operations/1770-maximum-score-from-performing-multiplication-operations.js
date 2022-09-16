df19dd0e5220c7acb25b0c025afc4a0f7fb7cfaf
Baek <74183179+Geunbaek@users.noreply.github.com>
2022-09-16 12:44:32 +0900

    Time: 321 ms (84.80%), Space: 84 MB (78.40%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.js
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.js
@@ -4,38 +4,15 @@
  * @return {number}
  */
 var maximumScore = function(nums, multipliers) {
-    const memo = new Map();
+    const [n, m] = [nums.length, multipliers.length];
+    const dp = Array.from({length: m + 1}, () => Array(m + 1).fill(0));
     
-    const recur = (depth, left, right) => {
-        if(depth >= multipliers.length){
-            return 0;
+    for(let i = m - 1; i >= 0; i --){
+        for(let left = i; left >= 0; left--){
+            const right = n - 1 - i + left
+            dp[i][left] = Math.max(multipliers[i] * nums[left] + dp[i + 1][left + 1],
+                                  multipliers[i] * nums[right] + dp[i + 1][left])
         }
-        
-        const key = `${left}-${right}`;
-        
-        if(memo[key]){
-            return memo[key];
-        }
-        
-        const l = multipliers[depth] * nums[left] + recur(depth + 1, left + 1, right);
-        const r = multipliers[depth] * nums[right] + recur(depth + 1, left, right - 1);
-        memo[key] = Math.max(l, r);
-        return memo[key];
     }
-    return recur(0, 0, nums.length - 1)
-};
-
-var maximumScore = function(nums, mults, st=0, en=nums.length-1, i=0, memo = new Map()) {
-    if( st > en || i >= mults.length )
-        return 0;
-    
-    var key = st + "," + en + ",";
-    if( memo.has(key) ) return memo.get(key);
-    
-    let a = nums[st]*mults[i] + maximumScore(nums, mults, st+1, en, i+1, memo);
-    let b = nums[en]*mults[i] + maximumScore(nums, mults, st, en-1, i+1, memo);
-    
-    let ans = Math.max(a,b);
-    memo.set(key, ans);
-    return ans;
+    return dp[0][0]
 };
