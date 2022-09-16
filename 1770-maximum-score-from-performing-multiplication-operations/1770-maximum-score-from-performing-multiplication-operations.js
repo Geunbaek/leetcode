cabb7ae25a650c2851918cedd4573b2cf6647fe5
Baek <74183179+Geunbaek@users.noreply.github.com>
2022-09-16 12:31:31 +0900

    Time: 8260 ms (11.20%), Space: 183.9 MB (5.60%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.js
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.js
@@ -3,6 +3,28 @@
  * @param {number[]} multipliers
  * @return {number}
  */
+var maximumScore = function(nums, multipliers) {
+    const memo = new Map();
+    
+    const recur = (depth, left, right) => {
+        if(depth >= multipliers.length){
+            return 0;
+        }
+        
+        const key = `${left}-${right}`;
+        
+        if(memo[key]){
+            return memo[key];
+        }
+        
+        const l = multipliers[depth] * nums[left] + recur(depth + 1, left + 1, right);
+        const r = multipliers[depth] * nums[right] + recur(depth + 1, left, right - 1);
+        memo[key] = Math.max(l, r);
+        return memo[key];
+    }
+    return recur(0, 0, nums.length - 1)
+};
+
 var maximumScore = function(nums, mults, st=0, en=nums.length-1, i=0, memo = new Map()) {
     if( st > en || i >= mults.length )
         return 0;
