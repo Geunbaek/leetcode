f27510fcf8b49bef79087a7524720e977679a781
Baek <74183179+Geunbaek@users.noreply.github.com>
2022-10-10 14:45:05 +0900

    Time: 40 ms (78.14%), Space: 13.9 MB (61.62%) - LeetHub

---
+++ 1328-break-a-palindrome/1328-break-a-palindrome.py
@@ -0,0 +1,21 @@
+class Solution:
+    def breakPalindrome(self, palindrome: str) -> str:
+        if len(palindrome) == 1:
+            return ""
+        palindrome = list(palindrome)
+        alphabet = [chr(ord('a') + i) for i in range(26)]
+        flag = False
+        for char in alphabet:
+            for i in range(len(palindrome)):
+                if len(palindrome) % 2 != 0 and i == len(palindrome) // 2:
+                    continue
+                if palindrome[i] > char:
+                    print(i, char)
+                    palindrome[i] = char
+                    return "".join(palindrome)
+        
+        for char in alphabet:
+            if palindrome[-1] != char:
+                palindrome[-1] = char
+                
+                return "".join(palindrome)
