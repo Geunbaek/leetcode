6e9e1d094d34f7a61bbf0cd47632a3165fd55f5c
Baek <74183179+Geunbaek@users.noreply.github.com>
2022-11-22 12:14:12 +0900

    Time: 31 ms (59.57%), Space: 2 MB (86.17%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.rs
+++ 0279-perfect-squares/0279-perfect-squares.rs
@@ -13,16 +13,5 @@ impl Solution {
         }
         dp[n as usize] as i32
     }
-}
-// import math
-// class Solution:
-//     def numSquares(self, n: int) -> int:
-//         dp = [float("inf") for _ in range(n + 1)]
-//         dp[0] = 0
-        
-//         for num in range(1, int(math.sqrt(n)) + 1):
-//             for i in range(num ** 2, n + 1):
-//                 dp[i] = min(dp[i] , dp[i - (num ** 2)] + 1)
-
-//         return dp[n]         
+}       
         
