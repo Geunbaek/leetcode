276bcf69faa14c47f8e3ae39c02981331f4d7e79
Baek <74183179+Geunbaek@users.noreply.github.com>
2022-12-15 19:47:51 +0900

    Time: 920 ms (11.11%), Space: 6 MB (24.07%) - LeetHub

---
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.rs
@@ -0,0 +1,28 @@
+use std::cmp;
+
+impl Solution {
+    pub fn longest_common_subsequence(text1: String, text2: String) -> i32 {
+        let text1 = String::from(" ") + &text1;
+        let text2 = String::from(" ") + &text2;
+        
+        let c:usize = text1.len();
+        let r:usize = text2.len();
+        
+        let mut dp: Vec<Vec<i32>> = vec![vec![0; c]; r];
+        
+        for y in 0..r {
+            for x in 0..c {
+                if x == 0 || y == 0 {
+                    continue;
+                }
+                
+                if text1.chars().nth(x).unwrap() == text2.chars().nth(y).unwrap() {
+                    dp[y][x] = dp[y - 1][x - 1] + 1;
+                } else {
+                    dp[y][x] = cmp::max(dp[y - 1][x], dp[y][x - 1]);
+                }
+            }
+        }
+        dp[r - 1][c - 1]
+    }
+}
