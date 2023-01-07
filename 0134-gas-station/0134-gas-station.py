0ff5c7d6699b0d39949c453bb04f2202c4f8f8dc
Baek <74183179+Geunbaek@users.noreply.github.com>
2023-01-07 14:02:43 +0900

    Time: 681 ms (90.86%), Space: 19.4 MB (23.25%) - LeetHub

---
+++ 0134-gas-station/0134-gas-station.py
@@ -0,0 +1,16 @@
+from collections import deque
+
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        if sum(gas) < sum(cost):
+            return -1
+        
+        s, f = 0, 0
+        for i in range(len(gas)):
+            if f + gas[i] < cost[i]:
+                s = i + 1
+                f = 0
+            else:
+                f += gas[i] - cost[i]
+        return s
+        
