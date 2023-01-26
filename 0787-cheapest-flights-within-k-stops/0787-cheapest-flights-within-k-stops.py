91efb5bbe3c87c4eac324d895af9f07928a03a13
Baek <74183179+Geunbaek@users.noreply.github.com>
2023-01-26 20:36:30 +0900

    Time: 9880 ms (5.02%), Space: 260.2 MB (5.12%) - LeetHub

---
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -0,0 +1,25 @@
+import heapq
+from collections import defaultdict
+
+class Solution:
+    def findCheapestPrice(self, n: int, flights: list[list[int]], src: int, dst: int, k: int) -> int:
+        edges = defaultdict(list)
+        visit = [0 for _ in range(n)]
+
+        for s, e, w in flights:
+            edges[s].append((w, e))
+
+        h = [(0, src, k)]
+        visit[src] += 1
+        while h:
+            weight, now, k = heapq.heappop(h)
+            if now == dst:
+                return weight
+            if k >= 0:
+                for w, e in edges[now]:
+                    if visit[e] <= n:
+                        alt = weight + w
+                        heapq.heappush(h, (alt, e, k-1))
+            visit[now] += 1
+
+        return -1
