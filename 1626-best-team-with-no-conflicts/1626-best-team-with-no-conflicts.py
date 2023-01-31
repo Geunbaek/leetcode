ffad09d4916a38a6dcbf42ac180ea3e4ef269233
Baek <74183179+Geunbaek@users.noreply.github.com>
2023-02-01 00:49:38 +0900

    Time: 407 ms (92.91%), Space: 14.1 MB (99.29%) - LeetHub

---
+++ 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.py
@@ -0,0 +1,6 @@
+class Solution:
+    def bestTeamScore(self, scores: List[int], ages: List[int]) -> int:
+        dp = [0 for _ in range(max(ages) + 1)]             
+        for score, age in sorted(zip(scores, ages)):                                              
+            dp[age] = score + max(dp[: age + 1])  
+        return max(dp)                         
