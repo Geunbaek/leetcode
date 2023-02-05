e9663d440b925e4ea8adfe9c7489235090ae80a6
Baek <74183179+Geunbaek@users.noreply.github.com>
2023-02-05 10:11:34 +0900

    Time: 158 ms (61.92%), Space: 15.2 MB (74.41%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -6,11 +6,20 @@ class Solution:
             return []
         
         answer = []
-        p_map = Counter(p)
-        for i in range(len(s) - len(p) + 1):
-            if Counter(s[i: i + len(p)]) == p_map:
-                answer.append(i)
-                
+        p_map = dict(Counter(p))
+        check = {}
+        length = 0
+   
+        for i in range(len(s)):
+            check[s[i]] = check.get(s[i], 0) + 1
+            length += 1
+            
+            if length > len(p):
+                check[s[i - len(p)]] = check.get(s[i - len(p)], 0) - 1
+                if check[s[i - len(p)]] == 0:
+                    del check[s[i - len(p)]]
+            if p_map == check:
+                answer.append(i - len(p) + 1)
         return answer
             
             
