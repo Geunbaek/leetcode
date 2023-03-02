60d9ade44874ea953fc00bb158679876578fd822
Baek <74183179+Geunbaek@users.noreply.github.com>
2023-03-02 20:52:17 +0900

    Time: 57 ms (86.52%), Space: 13.9 MB (96.79%) - LeetHub

--- 0443-string-compression/0443-string-compression.py
+++ 0443-string-compression/0443-string-compression.py
@@ -1,17 +1,25 @@
 class Solution:
     def compress(self, chars: List[str]) -> int:
-        i = 0
-        res = 0
-        while i < len(chars):
-            group_length = 1
-            while (i + group_length < len(chars)
-                   and chars[i + group_length] == chars[i]):
-                group_length += 1
-            chars[res] = chars[i]
-            res += 1
-            if group_length > 1:
-                str_repr = str(group_length)
-                chars[res:res+len(str_repr)] = list(str_repr)
-                res += len(str_repr)
-            i += group_length
-        return res
+        stack = []
+        
+        for char in chars:
+            if not stack:
+                stack.append([char, 1])
+                continue
+                
+            if stack[-1][0] == char:
+                stack[-1][1] += 1
+            else:
+                stack.append([char, 1])
+                
+        answer = []
+        for char, cnt in stack:
+            answer.append(char)
+            if cnt > 1:
+                answer.extend(list(str(cnt)))
+                
+                
+        for i in range(len(answer)):
+            chars[i] = answer[i]
+            
+        return len(answer)
