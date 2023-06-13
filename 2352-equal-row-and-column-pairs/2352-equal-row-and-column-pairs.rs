00457651c20712f1c5a502043d68cfa0751f3419
Baek <74183179+Geunbaek@users.noreply.github.com>
2023-06-13 20:01:02 +0900

    Time: 35 ms (15.79%), Space: 2.7 MB (81.58%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.rs
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.rs
@@ -1,28 +1,20 @@
 impl Solution {
-    pub fn get_row(grid: &Vec<Vec<i32>>, row: usize, c: usize) -> Vec<i32> {
-        return (0..c).map(|i| grid[row][i]).collect();
-    }
-    pub fn get_col(grid: &Vec<Vec<i32>>, col: usize, r: usize) -> Vec<i32> {
-        return (0..r).map(|i| grid[i][col]).collect();
-    }
-    pub fn is_equal(row: &Vec<i32>, col: &Vec<i32>) -> bool {
-        for i in 0..row.len() {
-            if row[i] != col[i] {
-                return false;
+    pub fn is_equal(grid: &Vec<Vec<i32>>, col:usize, row:usize, n:usize) -> bool {
+        for i in 0..n {
+            if grid[row][i] != grid[i][col] {
+                return false
             }
         }
         return true;
     }
     
     pub fn equal_pairs(grid: Vec<Vec<i32>>) -> i32 {
-        let (r, c) = (grid.len(), grid[0].len());
+        let n = grid.len();
         let mut answer = 0;
         
-        for y in 0..r {
-            let row = Solution::get_row(&grid, y, c);
-            for x in 0..c {
-                let col = Solution::get_col(&grid, x, r);
-                if Solution::is_equal(&row, &col) {
+        for y in 0..n {
+            for x in 0..n {
+                if Solution::is_equal(&grid, x, y, n) {
                     answer += 1;
                 }
             }
