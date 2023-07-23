dc982302792e06f329b4dd5585f010fbf90689f8
Baek <74183179+Geunbaek@users.noreply.github.com>
2023-07-23 13:40:25 +0900

    Time: 17 ms (20.00%), Space: 5.2 MB (20.00%) - LeetHub

--- 0894-all-possible-full-binary-trees/0894-all-possible-full-binary-trees.rs
+++ 0894-all-possible-full-binary-trees/0894-all-possible-full-binary-trees.rs
@@ -1,25 +1,56 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    def allPossibleFBT(self, n: int) -> List[Optional[TreeNode]]:
-        if n % 2 == 0:
-            return []
-        if n == 1:
-            return [TreeNode()]
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+// 
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
+use std::rc::Rc;
+use std::cell::RefCell;
+impl Solution {
+    pub fn all_possible_fbt(n: i32) -> Vec<Option<Rc<RefCell<TreeNode>>>> {
+        if n % 2 == 0 {
+            return vec![];
+        } 
         
-        answer = []
+        if n == 1 {
+            return vec![Some(Rc::new(RefCell::new(TreeNode::new(0))))];
+        }
         
-        for i in range(1, n, 2):
-            left = self.allPossibleFBT(i)
-            right = self.allPossibleFBT(n - i - 1)
+        let mut answer:Vec<Option<Rc<RefCell<TreeNode>>>> = vec![];
+        let mut i = 1;
+        
+        loop {
+            if i >= n {
+                break;
+            }
+            let left = Solution::all_possible_fbt(i);
+            let right = Solution::all_possible_fbt(n - i - 1);
             
-            for l in left:
-                for r in right:
-                    root = TreeNode(0, l, r)
-                    answer.append(root)
-                    
-        return answer
+            for l in &left {
+                for r in &right {
+                    let mut root = Rc::new(RefCell::new(TreeNode {
+                        val: 0,
+                        left: l.clone(),
+                        right: r.clone(),
+                    }));
+                    answer.push(Some(root));
+                }
+            }
+            i += 2;
+        }
+        return answer;
+    }
+}
+
