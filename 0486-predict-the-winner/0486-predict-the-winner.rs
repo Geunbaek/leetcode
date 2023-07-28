6ab4ae9082cce2bbff63277ca81e27a1bf84a5bf
Baek <74183179+Geunbaek@users.noreply.github.com>
2023-07-28 23:59:07 +0900

    Time: 1 ms (91.67%), Space: 2.2 MB (16.67%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.rs
+++ 0486-predict-the-winner/0486-predict-the-winner.rs
@@ -1,16 +1,24 @@
+use std::collections::HashMap;
+
 impl Solution {
-    fn max_diff(nums: &Vec<i32>, left:usize, right: usize) -> i32 {
+    fn max_diff(nums: &Vec<i32>, memo: &mut HashMap<(usize, usize), i32>, left:usize, right: usize) -> i32 {
+        if memo.contains_key(&(left, right)) {
+            return memo[&(left, right)];
+        }
+        
         if left == right {
             return nums[left];
         } else {
-            let left_score = nums[left] - Solution::max_diff(nums, left + 1, right);
-            let right_score = nums[right] - Solution::max_diff(nums, left, right - 1);
-            return std::cmp::max(left_score, right_score);
+            let left_score = nums[left] - Solution::max_diff(nums, memo, left + 1, right);
+            let right_score = nums[right] - Solution::max_diff(nums, memo, left, right - 1);
+            memo.insert((left, right), std::cmp::max(left_score, right_score));
+            return memo[&(left, right)];
         }
     }
     pub fn predict_the_winner(nums: Vec<i32>) -> bool {
         let n = nums.len();
+        let mut memo:HashMap<(usize, usize), i32> = HashMap::new();
         
-        return Solution::max_diff(&nums, 0, n - 1) >= 0;
+        return Solution::max_diff(&nums, &mut memo, 0, n - 1) >= 0;
     }
 }
