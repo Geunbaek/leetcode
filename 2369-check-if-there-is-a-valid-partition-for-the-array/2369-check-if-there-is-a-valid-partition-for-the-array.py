8c50fdcc30eebb4953c7f4573d8e2f80b1893409
Baek <74183179+Geunbaek@users.noreply.github.com>
2023-08-13 22:48:46 +0900

    Time: 876 ms (93.08%), Space: 30.5 MB (68.46%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.py
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.py
@@ -1,11 +1,7 @@
 class Solution:
     def validPartition(self, nums: List[int]) -> bool:
-        prefix_sum = [0]
         dp = [False for _ in range(len(nums) + 1)]
         
-        for num in nums:
-            prefix_sum.append(prefix_sum[-1] + num)
-            
         dp[0] = True
  
         for i in range(2, len(nums) + 1):
