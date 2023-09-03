68e06051d82f07417739d761ee5fef26cf4ac9f6
Baek <74183179+Geunbaek@users.noreply.github.com>
2023-09-03 11:08:06 +0900

    Time: 1 ms (43.97%), Space: 2.2 MB (22.69%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.rs
@@ -0,0 +1,16 @@
+impl Solution {
+    pub fn unique_paths(m: i32, n: i32) -> i32 {    
+        let m = m as usize;
+        let n = n as usize;
+        let mut dp = vec![vec![0; m + 1]; n + 1];
+        dp[1][1] = 1;
+        
+        for y in 1..=n {
+            for x in 1..=m {
+                dp[y][x] = std::cmp::max(dp[y][x], dp[y - 1][x] + dp[y][x - 1]);
+            }
+        }
+        print!("{:?}", dp);
+        return dp[n][m];
+    }
+}
