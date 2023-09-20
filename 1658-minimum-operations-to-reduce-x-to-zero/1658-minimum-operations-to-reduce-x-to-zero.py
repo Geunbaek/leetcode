b0d2fc450ec9ba33c13212d7fdadd880a3daf904
Baek <74183179+Geunbaek@users.noreply.github.com>
2023-09-20 20:57:21 +0900

    Time: 1434 ms (5.11%), Space: 33.8 MB (27.15%) - LeetHub

---
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.py
@@ -0,0 +1,31 @@
+class Solution:
+    def minOperations(self, nums: List[int], x: int) -> int:
+        def check_valid_range(left, right):
+            if left <= len(nums) <= right:
+                return True
+        
+            return False
+        expand_nums = nums * 2
+        n = len(expand_nums)
+        
+        prefix = [0]
+        
+        for i in range(n):
+            prefix.append(prefix[-1] + expand_nums[i])
+            
+        right = 0
+        answer = float('inf')
+        print(n)
+        for left in range(n + 1):
+            while right + 1 < n and prefix[right] - prefix[left] < x:
+                right += 1
+            
+            if prefix[right] - prefix[left] == x and right - left <= len(nums) and check_valid_range(left, right):
+                answer = min(answer, right - left)
+        
+        if answer == float('inf'):
+            return -1
+        
+        return answer
+            
+        
