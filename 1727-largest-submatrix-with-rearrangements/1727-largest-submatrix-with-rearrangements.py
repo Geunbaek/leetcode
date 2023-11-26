5abf3cdd8a1e15006084665758f46e100b5704d7
Baek <74183179+Geunbaek@users.noreply.github.com>
2023-11-26 10:19:09 +0900

    Time: 1232 ms (5.56%), Space: 43.4 MB (5.56%) - LeetHub

---
+++ 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.py
@@ -0,0 +1,29 @@
+class Solution:
+    def largestSubmatrix(self, matrix: List[List[int]]) -> int:
+        r = len(matrix)
+        c = len(matrix[0])
+        
+        for y in range(r):
+            for x in range(c):
+                if y == 0:
+                    continue
+                if matrix[y][x] == 0:
+                    continue
+                matrix[y][x] += matrix[y - 1][x]
+        for l in matrix:
+            print(l)
+        answer = 0
+        for y in range(r):
+            sorted_line = sorted(matrix[y], reverse=True)
+                
+            area = 0
+            _min = float('inf')
+            for x in range(c):
+                _min = min(_min, sorted_line[x])
+                area = max(area, (x + 1) * _min)
+            answer = max(answer, area)
+            
+        return answer
+                
+                
+        
