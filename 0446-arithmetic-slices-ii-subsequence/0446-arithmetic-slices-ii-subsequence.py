25415209b7223b1437258269a472d41c929651c0
Baek <74183179+Geunbaek@users.noreply.github.com>
2024-01-07 11:36:47 +0900

    Time: 607 ms (60.07%), Space: 72 MB (25.90%) - LeetHub

--- 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.py
+++ 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.py
@@ -8,10 +8,8 @@ class Solution:
             for x in range(y):
                 diff = nums[y] - nums[x]
                 ans += dp[x][diff]
-                # print(dp[x][diff])
                 dp[y][diff] += dp[x][diff] + 1
-        # for l in dp:
-        #     print(l)
+    
         return ans
         
         
