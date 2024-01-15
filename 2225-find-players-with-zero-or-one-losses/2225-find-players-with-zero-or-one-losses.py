8acf11b143cf21d0e03047655680dbbf90bfddce
Baek <74183179+Geunbaek@users.noreply.github.com>
2024-01-15 20:37:00 +0900

    Time: 1455 ms (77.03%), Space: 73.2 MB (9.04%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.py
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.py
@@ -1,23 +1,20 @@
+from collections import defaultdict
 class Solution:
     def findWinners(self, matches: List[List[int]]) -> List[List[int]]:
-        cache = defaultdict(int)
+        result = defaultdict(int)
+        participants = set()
+        for winner, loser in matches:
+            result[loser] += 1
+            participants.add(loser)
+            participants.add(winner)
         
-        for w, l in matches:
-            if w not in cache:
-                cache[w] = 0
-            cache[l] += 1
-            
         answer = [[], []]
-        
-        for key, val in cache.items():
-            if val > 1:
-                continue
-            answer[val].append(key)
-        
-        for i in range(2):
-            answer[i].sort()
+        for player in participants:
+            if result[player] == 0:
+                answer[0].append(player)
+            elif result[player] == 1:
+                answer[1].append(player)
+        for el in answer:
+            el.sort()
         return answer
-                
-                
             
-        
