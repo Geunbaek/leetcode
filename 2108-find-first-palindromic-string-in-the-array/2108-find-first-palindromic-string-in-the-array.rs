b23c77ab710093cff03f21faa666e681b2eea4bb
Baek <74183179+Geunbaek@users.noreply.github.com>
2024-02-14 03:30:36 +0900

    Time: 7 ms (32.35%), Space: 2.3 MB (29.41%) - LeetHub

---
+++ 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.rs
@@ -0,0 +1,26 @@
+impl Solution {
+    pub fn first_palindrome(words: Vec<String>) -> String {
+        fn is_palindrome(word: &String) -> bool {
+            let word_vec: Vec<char> = word.chars().collect();
+            
+            let (mut left, mut right) = (0_usize, word.len() - 1);
+            
+            while left < right {
+                if word_vec[left] == word_vec[right] {
+                    left += 1;
+                    right -= 1;
+                } else {
+                    return false;
+                }
+            }
+            return true;
+        }
+        
+        for word in words.iter() {
+            if is_palindrome(word) {
+                return word.to_string();
+            }
+        }
+        return String::from("");
+    }
+}
