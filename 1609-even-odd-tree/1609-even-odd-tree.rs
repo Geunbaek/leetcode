cac51768af2e6777dd0f97a06aa2d3f3cf291c2c
Baek <74183179+Geunbaek@users.noreply.github.com>
2024-02-29 20:28:48 +0900

    Time: 26 ms (50.00%), Space: 11.6 MB (100.00%) - LeetHub

--- 1609-even-odd-tree/1609-even-odd-tree.rs
+++ 1609-even-odd-tree/1609-even-odd-tree.rs
@@ -18,74 +18,50 @@
 // }
 use std::rc::Rc;
 use std::cell::RefCell;
+use std::collections::VecDeque;
+
 impl Solution {
     pub fn is_even_odd_tree(root: Option<Rc<RefCell<TreeNode>>>) -> bool {
-        fn dfs(trees: &mut Vec<Vec<i32>>, depth: usize, node: Option<Rc<RefCell<TreeNode>>>) {
-            match (node) {
-                Some(node) => {
-                    let node = node.borrow();
-                    trees[depth].push(node.val);
-                    dfs(trees, depth + 1, node.left.clone());
-                    dfs(trees, depth + 1, node.right.clone());
-                },
-                None => {}
-            }
-        }
-        
-        fn is_increasing(nodes: &Vec<i32>) -> bool {
-            let n = nodes.len();
-            
-            if nodes[0] % 2 == 0 {
-                return false;
-            }
-            
-            for i in 1..n {
-                if nodes[i] % 2 == 0 {
+        let mut q = VecDeque::new();
+        q.extend([root, None]);
+        let mut even = false;
+        let mut extreme = i32::MIN;
+        while !q.is_empty() {
+            let node = q.pop_front().unwrap();
+            if let Some(n) = node {
+                let n = n.borrow();
+                let val = n.val;
+                if (val % 2 == 0) != even {
                     return false;
                 }
-                if nodes[i] <= nodes[i - 1] {
-                    return false;
+                if even {
+                    if val < extreme {
+                        extreme = val;
+                    } else {
+                        return false;
+                    }
+                } else {
+                    if val > extreme {
+                        extreme = val;
+                    } else {
+                        return false;
+                    }
                 }
-            }
-            true
-        }
-        
-        fn is_decreasing(nodes: &Vec<i32>) -> bool {
-            let n = nodes.len();
-            
-            if nodes[0] % 2 != 0 {
-                return false;
-            }
-            
-            for i in 1..n {
-                if nodes[i] % 2 != 0 {
-                    return false;
+                if n.left.is_some() {
+                    q.push_back(n.left.clone());
                 }
-                if nodes[i] >= nodes[i - 1] {
-                    return false;
-                }
-            }
-            true
-        }
-         
-        let mut trees: Vec<Vec<i32>> = vec![vec![]; 1000001];
-        dfs(&mut trees, 0, root);
-        
-        for i in 0..1000001 {
-            if trees[i].len() == 0 {
-                break;
-            }
-            
-            if i % 2 == 0 {
-                if (!is_increasing(&trees[i])) {
-                    return false;
+                if n.right.is_some() {
+                    q.push_back(n.right.clone());
                 }
             } else {
-                if (!is_decreasing(&trees[i])) {
-                    return false;
+                // special case: None switches oddity
+                even = !even;
+                extreme = if even { i32::MAX } else { i32::MIN };
+                if !q.is_empty() {
+                    q.push_back(None);
                 }
             }
         }
-        true   
+        true
     }
 }
