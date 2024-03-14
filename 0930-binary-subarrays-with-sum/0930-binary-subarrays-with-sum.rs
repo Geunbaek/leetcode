4485e638106e0c2ac8fed71f2ffa3c47dec2fe82
Baek <74183179+Geunbaek@users.noreply.github.com>
2024-03-14 20:23:44 +0900

    Time: 12 ms (12.50%), Space: 3.1 MB (37.50%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.rs
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.rs
@@ -3,13 +3,12 @@ use std::collections::HashMap;
 impl Solution {
     pub fn num_subarrays_with_sum(nums: Vec<i32>, goal: i32) -> i32 {
         let n = nums.len();
-        let mut left = 0_usize;
         let mut sum = 0;
         let mut answer = 0;
         let mut cache: HashMap<i32, i32> = HashMap::new();
         
-        for right in 0..n {
-            sum += nums[right];
+        for i in 0..n {
+            sum += nums[i];
             
             if sum == goal {
                 answer += 1;
