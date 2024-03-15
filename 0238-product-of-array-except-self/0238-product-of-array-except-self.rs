cbe01e8ecfde86db9ac3106e1bfc18ce56dcf6d5
Baek <74183179+Geunbaek@users.noreply.github.com>
2024-03-15 19:53:38 +0900

    Time: 19 ms (5.16%), Space: 3.3 MB (20.82%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.rs
@@ -0,0 +1,27 @@
+impl Solution {
+    pub fn product_except_self(nums: Vec<i32>) -> Vec<i32> {
+        let n = nums.len();
+        
+        let mut l = vec![1];
+        let mut r = vec![1];
+        
+        let mut answer = vec![];
+        
+        for i in 0..n {
+            let mul = l[i] * nums[i];
+            l.push(mul);
+        }
+        
+        for i in (0..n).rev() {
+            let mul = r[n - i - 1] * nums[i];
+            r.push(mul);
+        }
+        println!("{l:?} {r:?}");
+        for i in 1..=n {
+            let left = l[i - 1] / l[0];
+            let right = r[n - i] / r[0];
+            answer.push(left * right);
+        }
+        answer
+    }
+}
