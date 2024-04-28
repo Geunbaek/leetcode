fbe4a36b8fe9f0fc05f82087cc662ba6d1d3157d
Baek <74183179+Geunbaek@users.noreply.github.com>
2024-04-28 15:38:22 +0900

    Time: 955 ms (5.15%), Space: 41.5 MB (80.76%) - LeetHub

--- 0834-sum-of-distances-in-tree/0834-sum-of-distances-in-tree.py
+++ 0834-sum-of-distances-in-tree/0834-sum-of-distances-in-tree.py
@@ -1,25 +1,32 @@
-class Solution(object):
-    def sumOfDistancesInTree(self, N, edges):
-        graph = collections.defaultdict(set)
-        for u, v in edges:
-            graph[u].add(v)
-            graph[v].add(u)
-
-        count = [1] * N
-        ans = [0] * N
-        def dfs(node = 0, parent = None):
+class Solution:
+    def sumOfDistancesInTree(self, n: int, edges: List[List[int]]) -> List[int]:
+        
+        def dfs(node, parent):
             for child in graph[node]:
                 if child != parent:
                     dfs(child, node)
                     count[node] += count[child]
                     ans[node] += ans[child] + count[child]
-
-        def dfs2(node = 0, parent = None):
+                    
+        def dfs2(node, parent):
             for child in graph[node]:
                 if child != parent:
-                    ans[child] = ans[node] - count[child] + N - count[child]
+                    ans[child] = ans[node] - count[child] + n - count[child]
                     dfs2(child, node)
-
-        dfs()
-        dfs2()
+        
+        
+        graph = [[] for _ in range(n)]
+        
+        for u, v in edges:
+            graph[u].append(v)
+            graph[v].append(u)
+            
+        count = [1] * n
+        ans = [0] * n
+        
+        dfs(0, -1)
+        dfs2(0, -1)
         return ans
+        
+        
+        
