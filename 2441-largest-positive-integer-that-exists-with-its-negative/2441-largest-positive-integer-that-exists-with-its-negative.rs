4b442b0713ba19446fe584ac235c7184410dbe87
Baek <74183179+Geunbaek@users.noreply.github.com>
2024-05-02 13:22:11 +0900

    Time: 5 ms (46.67%), Space: 2.2 MB (46.67%) - LeetHub

--- 2441-largest-positive-integer-that-exists-with-its-negative/2441-largest-positive-integer-that-exists-with-its-negative.rs
+++ 2441-largest-positive-integer-that-exists-with-its-negative/2441-largest-positive-integer-that-exists-with-its-negative.rs
@@ -1,15 +1,19 @@
+use std::collections::HashMap;
+
 impl Solution {
     pub fn find_max_k(nums: Vec<i32>) -> i32 {
         let mut max = -1;
-        
+        let mut cache: HashMap<i32, i32> = HashMap::new();
         for num in nums.iter() {
-            if nums.contains(&-num) {
+            if cache.contains_key(&-num) {
                 if *num > 0 {
                     max = std::cmp::max(max, *num);
                 } else {
                     max = std::cmp::max(max, -*num);
                 }
             }
+            cache.entry(*num).and_modify(|x| *x += 1).or_insert(1);
+            
         }
         max
     }
