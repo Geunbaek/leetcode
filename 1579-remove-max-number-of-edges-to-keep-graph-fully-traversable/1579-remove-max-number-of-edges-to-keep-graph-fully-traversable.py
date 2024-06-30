482bc6f983f98d286bb9c1dd0787a83eded8fd0d
Baek <74183179+Geunbaek@users.noreply.github.com>
2024-06-30 13:17:18 +0900

    Time: 1735 ms (30.31%), Space: 56.2 MB (85.07%) - LeetHub

--- 1579-remove-max-number-of-edges-to-keep-graph-fully-traversable/1579-remove-max-number-of-edges-to-keep-graph-fully-traversable.py
+++ 1579-remove-max-number-of-edges-to-keep-graph-fully-traversable/1579-remove-max-number-of-edges-to-keep-graph-fully-traversable.py
@@ -10,36 +10,35 @@ class Solution:
             bp = find(p, b)
             p[ap] = bp
             
+        
         p1 = [i for i in range(n + 1)]
         p2 = [i for i in range(n + 1)]
-        v1 = [0 for _ in range(len(edges))]
-        c1 = 0
-        c2 = 0
+        c = len(edges)
+        edges.sort(reverse=True)
         
-        edges.sort(key = lambda x: -x[0])
-        
-        for i, (t, u, v) in enumerate(edges):
+        for t, u, v in edges:
+            if t == 3:
+                if find(p1, u) != find(p1, v) and find(p2, u) != find(p2, v):
+                    union(p1, u, v)
+                    union(p2, u, v)
+                    c -= 1
             if t == 2:
-                continue
-                
-            if find(p1, u) != find(p1, v):
-                union(p1, u, v)
-                v1[i] = 1
-                c1 += 1
-        
-        for i, (t, u, v) in enumerate(edges):
+                if find(p2, u) != find(p2, v):
+                    union(p2, u, v)
+                    c -= 1
             if t == 1:
-                continue
-                
-            if find(p2, u) != find(p2, v):
-                union(p2, u, v)
-                v1[i] = 1
-                c2 += 1
-                
-        count = v1.count(1)
-      
-        if c1 != n - 1 or c2 != n - 1:
-            return -1
-        return len(v1) - count
+                if find(p1, u) != find(p1, v):
+                    union(p1, u, v)
+                    c -= 1
         
+        root1 = find(p1, 1)
+        root2 = find(p2, 1)
+        
+        for i in range(2, n + 1):
+            if root1 != find(p1, i):
+                return -1
+            if root2 != find(p2, i):
+                return -1
+        
+        return c
         
