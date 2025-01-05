14d111fd42e53d0c1a74765a511fbb48b6822fca
Baek <74183179+Geunbaek@users.noreply.github.com>
2025-01-05 16:53:58 +0900

    Time: 1655 ms (5.16%) | Memory: 41.3 MB (42.25%) - LeetSync

---
+++ 2465-shifting-letters-ii/shifting-letters-ii.py
@@ -0,0 +1,40 @@
+class Solution:
+    def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
+        n = len(s)
+
+        letters = [ord(c) - ord("a") for c in s]
+
+        dp = [0 for _ in range(n + 1)]
+
+        for s, e, d in shifts:
+            if d == 0:
+                dp[s] -= 1
+                dp[e + 1] += 1
+            else:
+                dp[s] += 1
+                dp[e + 1] -= 1
+
+        prefix_sum = [0]
+
+        for i in range(n):
+            prefix_sum.append(prefix_sum[-1] + dp[i])
+
+        answer = []
+
+        for i in range(1, n + 1):
+            letter = letters[i - 1] + prefix_sum[i]
+            print(letter)
+
+            while letter < 0:
+                letter += 26
+
+            while letter >= 26:
+                letter -= 26
+
+            answer.append(chr(letter + ord('a')))
+
+        return "".join(answer)
+
+
+
+        
