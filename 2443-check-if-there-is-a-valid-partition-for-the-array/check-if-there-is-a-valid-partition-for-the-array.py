a9b53d375c9761ae47d3a4b474e9f289c5437b53
Baek <74183179+Geunbaek@users.noreply.github.com>
2025-03-05 23:43:17 +0900

    Time: 104 ms (84.13%) | Memory: 33.5 MB (54.27%) - LeetSync

---
+++ 2443-check-if-there-is-a-valid-partition-for-the-array/check-if-there-is-a-valid-partition-for-the-array.py
@@ -0,0 +1,18 @@
+class Solution:
+    def validPartition(self, nums: List[int]) -> bool:
+        dp = [False for _ in range(len(nums) + 1)]
+        dp[0] = True
+ 
+        for i in range(2, len(nums) + 1):
+            if dp[i - 2]:
+                if nums[i - 1] == nums[i - 2]:
+                    dp[i] = True
+            if i >= 3 and dp[i - 3]:
+                if nums[i - 1] == nums[i - 2] and nums[i - 1] == nums[i - 3]:
+                    dp[i] = True
+                if nums[i -1] == nums[i - 2] + 1 and nums[i - 1] == nums[i - 3] + 2:
+                    dp[i] = True
+        
+        return dp[-1]
+            
+        
