3e3a727d1c20c69f201137602113a0c0d69b9d02
Baek <74183179+Geunbaek@users.noreply.github.com>
2025-03-16 12:14:25 +0900

    Time: 1106 ms (24.95%) | Memory: 21.9 MB (49.42%) - LeetSync

---
+++ 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.py
@@ -0,0 +1,20 @@
+class Solution:
+    def repairCars(self, ranks: List[int], cars: int) -> int:
+        def can_repair_cars_in_time(time):
+            repair_cars = 0
+
+            for rank in ranks:
+                repair_cars += math.floor(math.sqrt(time // rank))
+
+            return repair_cars >= cars
+        
+        left, right = 0, 100_000_000_000_000
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if can_repair_cars_in_time(mid):
+                right = mid - 1
+            else:
+                left = mid + 1
+        return left
