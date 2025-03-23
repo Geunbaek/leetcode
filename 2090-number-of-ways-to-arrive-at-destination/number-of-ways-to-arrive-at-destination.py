49c260032e66cf63c90677b330d1b41916eb23ab
Baek <74183179+Geunbaek@users.noreply.github.com>
2025-03-23 14:14:54 +0900

    Time: 15 ms (90.31%) | Memory: 25.2 MB (37.81%) - LeetSync

--- 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.py
+++ 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.py
@@ -2,28 +2,25 @@ class Solution:
     def countPaths(self, n: int, roads: List[List[int]]) -> int:
         def dij(start):
             h = [(0, start)]
-            dists = [float('inf') for _ in range(n)]
-            path_count = [0 for _ in range(n)]
-
-            path_count[start] = 1
-            dists[start] = 0
+            times = [float("inf") for _ in range(n)]
+            paths = [0 for _ in range(n)]
+            paths[start] = 1
+            times[start] = 0
 
             while h:
-                cost, now = heappop(h)
+                time, now = heappop(h)
 
-                if dists[now] < cost:
+                if times[now] < time:
                     continue
 
-                for _next, _cost in graph[now]:
-                    if dists[_next] > cost + _cost:
-                        dists[_next] = cost + _cost
-                        path_count[_next] = path_count[now]
-                        heapq.heappush(h, (cost + _cost, _next))
-                    elif dists[_next] == cost + _cost:
-                        path_count[_next] = (path_count[_next] + path_count[now]) % 1_000_000_007
-            
-            return dists, path_count
-
+                for _next, cost in graph[now]:
+                    if times[_next] > cost + time:
+                        heappush(h, (cost + time, _next))
+                        paths[_next] = paths[now]
+                        times[_next] = cost + time
+                    elif times[_next] == cost + time:
+                        paths[_next] = (paths[_next] + paths[now]) % 1_000_000_007
+            return paths[n - 1]
 
         graph = [[] for _ in range(n)]
 
@@ -31,5 +28,4 @@ class Solution:
             graph[u].append((v, c))
             graph[v].append((u, c))
         
-        dists, path_count = dij(0)
-        return path_count[n - 1]
+        return dij(0)
