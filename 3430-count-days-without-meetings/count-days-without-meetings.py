9e2adfd4df8ba5f879227ffd2bc5a7c5530efd5e
Baek <74183179+Geunbaek@users.noreply.github.com>
2025-03-24 12:34:09 +0900

    Time: 246 ms (16.62%) | Memory: 52.8 MB (79.34%) - LeetSync

---
+++ 3430-count-days-without-meetings/count-days-without-meetings.py
@@ -0,0 +1,20 @@
+class Solution:
+    def countDays(self, days: int, meetings: List[List[int]]) -> int:
+        meetings.sort()
+        stack = []
+
+        for start, end in meetings:
+            if not stack:
+                stack.append((start, end))
+                continue
+
+            last_start, last_end = stack.pop()
+            if last_start <= start <= last_end:
+                stack.append((last_start, max(last_end, end)))
+            else:
+                stack.append((last_start, last_end))
+                stack.append((start, end))
+        print(stack)
+        for s, e in stack:
+            days -= (e - s) + 1
+        return days
