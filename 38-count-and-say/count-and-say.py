877f3d565dcd1858997608c5082b06abe0381aae
Baek <74183179+Geunbaek@users.noreply.github.com>
2025-04-18 11:15:34 +0900

    Time: 7 ms (73.96%) | Memory: 18 MB (20.58%) - LeetSync

---
+++ 38-count-and-say/count-and-say.py
@@ -0,0 +1,23 @@
+class Solution:
+    def countAndSay(self, n: int) -> str:   
+        def compress(s):
+            stack = []
+
+            for c in s:
+                if not stack:
+                    stack.append((1, c))
+                    continue
+
+                last_count, last_char = stack[-1]
+                if last_char != c:
+                    stack.append((1, c))
+                else:
+                    stack.pop()
+                    stack.append((last_count + 1, c))
+            return "".join(map(lambda x: f"{x[0]}{x[1]}", stack))
+
+        dp = ["1"]
+
+        for i in range(1, n):
+            dp.append(compress(dp[i - 1]))
+        return dp[n - 1]
