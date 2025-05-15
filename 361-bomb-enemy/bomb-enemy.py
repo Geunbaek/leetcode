9f71eed7429d37284b04ba7006f9b73a82c87fbb
Baek <74183179+Geunbaek@users.noreply.github.com>
2025-05-15 11:30:25 +0900

    Time: 1422 ms (5.11%) | Memory: 171.9 MB (5.68%) - LeetSync

---
+++ 361-bomb-enemy/bomb-enemy.py
@@ -0,0 +1,41 @@
+class Solution:
+    def maxKilledEnemies(self, grid: List[List[str]]) -> int:
+        def dfs(x, y, killed, d):
+            if not (0 <= x < c and 0 <= y < r):
+                return killed
+
+            if grid[y][x] == "W":
+                return killed
+
+            if (x, y) in memo and d in memo[(x, y)]:
+                return memo[(x, y)][d]
+            
+            memo[(x, y)] = memo.get((x, y), {})
+            nx = x + dx[d]
+            ny = y + dy[d]
+            if grid[y][x] == "E":
+                memo[(x, y)][d] = dfs(nx, ny, killed + 1, d)
+            else:
+                memo[(x, y)][d] = dfs(nx, ny, killed, d)
+            return memo[(x, y)][d]
+
+        r, c = len(grid), len(grid[0])
+        dx = [-1, 0, 1, 0]
+        dy = [0, -1, 0, 1]
+        
+        memo = {}
+        maxKilledEnemies = 0
+
+        for y in range(r):
+            for x in range(c):
+                if grid[y][x] == "0":
+                    killedEnemies = 0
+
+                    for i in range(4):
+                        killedEnemies += dfs(x, y, 0, i)
+
+                    maxKilledEnemies = max(maxKilledEnemies, killedEnemies)
+
+        # for key, val in memo.items():
+        #     print(key, val)
+        return maxKilledEnemies
