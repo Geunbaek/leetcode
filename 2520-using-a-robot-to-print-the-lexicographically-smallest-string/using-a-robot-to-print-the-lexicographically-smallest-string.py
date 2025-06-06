c7b7507fbdb811f1f6814e8feaa11c0976604bfe
Baek <74183179+Geunbaek@users.noreply.github.com>
2025-06-06 13:01:53 +0900

    Time: 627 ms (29.39%) | Memory: 20.7 MB (59.16%) - LeetSync

---
+++ 2520-using-a-robot-to-print-the-lexicographically-smallest-string/using-a-robot-to-print-the-lexicographically-smallest-string.py
@@ -0,0 +1,41 @@
+class Solution:
+    def robotWithString(self, s: str) -> str:
+        def get_min_char():
+            return min(counter.keys())
+        
+        def minus_char(char):
+            counter[char] -= 1
+            if counter[char] == 0:
+                del counter[char]
+        
+        counter = Counter(s)
+        q = deque(s)
+        stack = []
+        answer = []
+
+        while counter:
+            c = get_min_char()
+            while q:
+                first = q.popleft()
+                if not stack:
+                    stack.append(first)
+                    minus_char(first)
+                    continue
+
+                if stack[-1] > c:
+                    stack.append(first)
+                    minus_char(first)
+                else:
+                    q.appendleft(first)
+                    break
+            if counter:
+                c = get_min_char()
+                while stack and c >= stack[-1]:
+                    answer.append(stack.pop())
+
+        while stack:
+            answer.append(stack.pop())
+        while q:
+            answer.append(q.popleft())
+
+        return "".join(answer)
