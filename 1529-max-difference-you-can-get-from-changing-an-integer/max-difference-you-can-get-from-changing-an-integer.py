fd387a6551bb4ca0c8130d88b3b4cd7039516aee
Baek <74183179+Geunbaek@users.noreply.github.com>
2025-06-15 12:21:34 +0900

    Time: 7 ms (9.69%) | Memory: 17.6 MB (88.27%) - LeetSync

---
+++ 1529-max-difference-you-can-get-from-changing-an-integer/max-difference-you-can-get-from-changing-an-integer.py
@@ -0,0 +1,26 @@
+class Solution:
+    def maxDiff(self, num: int) -> int:
+        def change_num(num, target, new):
+            str_num = list(str(num))
+
+            for i in range(len(str_num)):
+                if str_num[i] == target:
+                    str_num[i] = new
+
+            return int("".join(str_num))
+        
+        _min = float('inf')
+        _max = float('-inf')
+
+        for i in range(10):
+            new_max_num = change_num(num, str(i), '9')
+            new_min_num1 = change_num(num, str(i), '1')
+            new_min_num2 = change_num(num, str(i), '0')
+            if len(str(new_min_num1)) != len(str(new_min_num2)) or new_min_num2 == 0:
+                _min = min(_min, new_min_num1)
+            else:
+                _min = min(_min, new_min_num2)
+                
+            _max = max(_max, new_max_num)
+        print(_max, _min)
+        return _max - _min
