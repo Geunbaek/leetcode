9d3a4cc880f748c3a97cac472279dc7877e3d8c6
Baek <74183179+Geunbaek@users.noreply.github.com>
2025-06-25 12:29:20 +0900

    Time: 2966 ms (59.70%) | Memory: 26 MB (23.38%) - LeetSync

---
+++ 2150-kth-smallest-product-of-two-sorted-arrays/kth-smallest-product-of-two-sorted-arrays.py
@@ -0,0 +1,39 @@
+class Solution:
+    def kthSmallestProduct(self, nums1: List[int], nums2: List[int], k: int) -> int:
+        def binary_search(arr, target):
+            l, r = 0, len(arr) - 1
+
+            while l <= r:
+                mid = (l + r) // 2
+                if arr[mid] <= target:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+            return l
+        
+        def calc_less_count(target):
+            less_count = 0
+
+            for num in nums1:
+                if num == 0:
+                    if target >= 0:
+                        less_count += len(nums2)
+                    continue
+                
+                if num < 0:
+                    less_count += len(nums2) - bisect_left(nums2, ceil(target / num))
+                else:
+                    less_count += bisect_right(nums2, target // num)
+            return less_count
+
+        MIN = -(100_000 * 100_000)
+        MAX = (100_000 * 100_000)
+        l, r = MIN, MAX
+
+        while l <= r:
+            mid = (l + r) // 2
+            if calc_less_count(mid) >= k:
+                r = mid - 1
+            else:
+                l = mid + 1
+        return l
