a6210546d205e7b677f0402f0568bd3fde686749
Baek <74183179+Geunbaek@users.noreply.github.com>
2025-06-26 11:00:01 +0900

    Time: 1 ms (100.00%) | Memory: 55.7 MB (25.00%) - LeetSync

---
+++ 2395-longest-binary-subsequence-less-than-or-equal-to-k/longest-binary-subsequence-less-than-or-equal-to-k.ts
@@ -0,0 +1,19 @@
+function longestSubsequence(s: string, k: number): number {
+    let num = 0;
+    let answer = 0;
+    const bits = k.toString(2).length;
+
+    for (let i = 0; i < s.length; i ++) {
+        const c = s[s.length - i - 1];
+        if (c === "1") {
+            if (i < bits && num + (1 << i) <= k) {
+                answer += 1;
+                num += 1 << i;
+            }
+        } else {
+            answer += 1;
+        }
+    }
+
+    return answer
+};
