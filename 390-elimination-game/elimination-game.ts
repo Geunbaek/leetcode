27078d88e336aedc50b4b62f5ccfc1b6d3e3c638
Baek <74183179+Geunbaek@users.noreply.github.com>
2025-07-05 21:43:05 +0900

    Time: 2 ms (82.35%) | Memory: 59.7 MB (88.24%) - LeetSync

---
+++ 390-elimination-game/elimination-game.ts
@@ -0,0 +1,17 @@
+function lastRemaining(n: number): number {
+    const l = Math.floor(Math.log2(n));
+
+    let answer = 1;
+
+    for (let i = 0; i < l; i ++) {
+        if (i % 2 === 0) {
+            answer += Math.pow(2, i);
+        } else {
+            if (n % 2 !== 0) {
+                answer += Math.pow(2, i);
+            }
+        }
+        n = Math.floor(n / 2);
+    }
+    return answer
+};
