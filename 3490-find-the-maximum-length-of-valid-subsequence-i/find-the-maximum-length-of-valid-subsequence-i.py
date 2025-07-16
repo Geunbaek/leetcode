942da34bb2f0ddb2085999291b57cdd822159762
Baek <74183179+Geunbaek@users.noreply.github.com>
2025-07-16 12:05:28 +0900

    Time: 51 ms (78.29%) | Memory: 40.1 MB (16.28%) - LeetSync

--- 3490-find-the-maximum-length-of-valid-subsequence-i/find-the-maximum-length-of-valid-subsequence-i.py
+++ 3490-find-the-maximum-length-of-valid-subsequence-i/find-the-maximum-length-of-valid-subsequence-i.py
@@ -1,24 +1,28 @@
 class Solution:
     def maximumLength(self, nums: List[int]) -> int:
-        evens = list(filter(lambda x: x % 2 == 0, nums))
-        odds = list(filter(lambda x: x % 2 != 0, nums))
-
+        evens = []    
         stack = []
+        odds = []
 
         for num in nums:
-            if not stack:
-                stack.append(num)
-                continue
+            if num % 2 == 0:
+                evens.append(num)
+
+                if not stack:
+                    stack.append(num)
+                    continue
+                if stack[-1] % 2 != 0:
+                    stack.append(num)
             
-            if stack[-1] % 2 == 0 and num % 2 != 0:
-                stack.append(num)
+            else:
+                odds.append(num)
 
-            if stack[-1] % 2 != 0 and num % 2 == 0:
-                stack.append(num)
+                if not stack:
+                    stack.append(num)
+                    continue
+                if stack[-1] % 2 == 0:
+                    stack.append(num)
 
-        print(evens)
-        print(odds)
-        print(stack)
         return max(
             len(evens), len(odds), len(stack)
         )
