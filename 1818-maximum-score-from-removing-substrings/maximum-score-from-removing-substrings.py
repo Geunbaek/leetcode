e0467e29a90c3103f85dadb1b25a9ba16618b218
Baek <74183179+Geunbaek@users.noreply.github.com>
2025-07-23 10:06:08 +0900

    Time: 231 ms (49.03%) | Memory: 19.5 MB (38.06%) - LeetSync

---
+++ 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.py
@@ -0,0 +1,26 @@
+class Solution:
+    def maximumGain(self, s: str, x: int, y: int) -> int:
+        def remove_sub(word, sub, p):
+            stack = []
+            point = 0
+
+            for char in word:
+                if stack and stack[-1] == sub[0] and char == sub[1]:
+                    stack.pop()
+                    point += p
+                    continue
+                stack.append(char)
+            return "".join(stack), point
+
+        answer = 0
+        sub = [(x, "ab"), (y, "ba")]
+        sub.sort(reverse=True)
+
+        for p, sub_s in sub:
+            s, point = remove_sub(s, sub_s, p)
+            print(s)
+            answer += point
+        
+        return answer
+
+        
