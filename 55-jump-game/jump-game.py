aed7570abea28f020fdc4a60e5c9f43753a34414
Baek <74183179+Geunbaek@users.noreply.github.com>
2025-07-24 10:42:06 +0900

    Time: 14 ms (80.46%) | Memory: 18.8 MB (13.85%) - LeetSync

--- 55-jump-game/jump-game.py
+++ 55-jump-game/jump-game.py
@@ -1,26 +1,11 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
         n = len(nums)
-        h = [(0, 0)]
-        visited = set([0])
+        now = n - 1
 
-        while h:
-            now, cnt = heappop(h)
-            now = -now
+        for i in range(n - 2, -1, -1):
+            if i + nums[i] >= now:
+                now = i
 
-            if now == n - 1:
-                return True
-
-            for i in range(1, nums[now] + 1):
-                _next = now + i
-                if _next >= n:
-                    continue
-                if _next in visited:
-                    continue 
-                visited.add(_next)
-                heappush(h, (-_next, cnt + 1))
-        return False
-
-            
-
+        return now == 0
 
