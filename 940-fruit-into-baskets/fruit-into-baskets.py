1751a331ab65623ec9058ebe96eab60c06520a5e
Baek <74183179+Geunbaek@users.noreply.github.com>
2025-08-04 10:02:23 +0900

    Time: 183 ms (59.92%) | Memory: 23.7 MB (42.94%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.py
+++ 940-fruit-into-baskets/fruit-into-baskets.py
@@ -14,19 +14,19 @@ class Solution:
 
         total = 0
         answer = 0
-        right = 0
-        for left in range(n):
-            while right < n and canHold(basket, fruits[right], 2):
-                basket[fruits[right]] = basket.get(fruits[right], 0) + 1
-                total += 1
-                right += 1
+        left = 0
+        for right in range(n):
+            basket[fruits[right]] = basket.get(fruits[right], 0) + 1
+            total += 1
+
+            while len(basket) > 2:
+                basket[fruits[left]] -= 1
+                if basket[fruits[left]] == 0:
+                    del basket[fruits[left]]
+                total -= 1
+                left += 1
             
             answer = max(answer, total)
-            basket[fruits[left]] -= 1
-            total -= 1
-            if basket[fruits[left]] == 0:
-                del basket[fruits[left]]
-
 
         return answer
 
