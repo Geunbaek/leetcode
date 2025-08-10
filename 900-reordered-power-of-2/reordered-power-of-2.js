93ccf6d21e98040870964001f97595188c574709
Baek <74183179+Geunbaek@users.noreply.github.com>
2025-08-10 12:59:58 +0900

    Time: 1 ms (77.78%) | Memory: 55.2 MB (74.07%) - LeetSync

--- 900-reordered-power-of-2/reordered-power-of-2.js
+++ 900-reordered-power-of-2/reordered-power-of-2.js
@@ -3,31 +3,15 @@
  * @return {boolean}
  */
 var reorderedPowerOf2 = function(n) {
-    function recur(path) {
-        if (path.length >= length) {
-            const result = parseInt(path.join(""));
-            const log2Result = Math.log2(result);
-            const isValid = Math.floor(log2Result) === log2Result;
-            return isValid
-        }
+    const getSortedNum = (num) => num.toString().split("").sort().join("");
+    const target = getSortedNum(n);
 
-        for (let i = 0; i < 10; i ++) {
-            if (counter[i] === 0) continue;
-            if (path.length === 0 && i === 0) continue;
-            counter[i] -= 1;
-            const result = recur([...path, i]);
-            if (result) return result
-            counter[i] += 1;
-        }
-        return false
+    const poweredSet = new Set();
+
+    for(let i = 0; i < 31; i++) {
+        const poweredNum = 1 << i;
+        poweredSet.add(getSortedNum(poweredNum));
     }
 
-    const numberStr = n.toString();
-    const {length} = numberStr;
-    const counter = new Array(10).fill(0);
-
-    for (let i = 0; i < length; i ++) {
-        counter[numberStr[i]] += 1;
-    }
-    return recur([]);
+    return poweredSet.has(target)
 };
