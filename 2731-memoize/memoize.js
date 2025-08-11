d3e4de3fd7efa3c7c92679f3fa4f45e4132ac67d
Baek <74183179+Geunbaek@users.noreply.github.com>
2025-08-11 11:56:10 +0900

    Time: 258 ms (65.71%) | Memory: 96.8 MB (8.06%) - LeetSync

--- 2731-memoize/memoize.js
+++ 2731-memoize/memoize.js
@@ -3,21 +3,15 @@
  * @return {Function}
  */
 function memoize(fn) {
-    
-    const cache = {};
-  
-   return function(...args) {
-    const key = JSON.stringify(args);
-    
-    if (key in cache) {
-      return cache[key];
+    const cache = new Map();
+    return function(...args) {
+        const stringArgs = args.join()
+        if (cache.has(stringArgs)){
+            return cache.get(stringArgs)
+        }
+        cache.set(stringArgs, fn(...args));
+        return cache.get(stringArgs);
     }
-    
-    const result = fn.apply(this, args);
-    cache[key] = result;
-    
-    return result;
-  }
 }
 
 
