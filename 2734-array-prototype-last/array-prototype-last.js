71f9ba3faa43d63301399abd4fd446275b9f4667
Baek <74183179+Geunbaek@users.noreply.github.com>
2025-08-11 14:37:09 +0900

    Time: 37 ms (75.44%) | Memory: 53.6 MB (28.55%) - LeetSync

---
+++ 2734-array-prototype-last/array-prototype-last.js
@@ -0,0 +1,12 @@
+/**
+ * @return {null|boolean|number|string|Array|Object}
+ */
+Array.prototype.last = function() {
+    const {length} = this;
+    return length !== 0 ? this[length - 1] : -1
+};
+
+/**
+ * const arr = [1, 2, 3];
+ * arr.last(); // 3
+ */
