3be13cead0c81d861bd1521b6a58b15db19c4aff
Baek <74183179+Geunbaek@users.noreply.github.com>
2025-08-11 15:03:30 +0900

    Time: 166 ms (12.85%) | Memory: 91.1 MB (38.46%) - LeetSync

--- 2759-flatten-deeply-nested-array/flatten-deeply-nested-array.js
+++ 2759-flatten-deeply-nested-array/flatten-deeply-nested-array.js
@@ -4,5 +4,14 @@
  * @return {Array}
  */
 var flat = function (arr, n) {
-    return arr.flat(n)
+    const ret = [];
+    for (let el of arr) {
+        if (Array.isArray(el) && n !== 0) {
+            ret.push(...flat(el, n - 1));
+            continue
+        }
+        ret.push(el)
+    }
+
+    return ret
 };
