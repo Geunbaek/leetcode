f449cbd2a3b2654300f4798e4f468b00a8cf3031
Baek <74183179+Geunbaek@users.noreply.github.com>
2025-08-12 16:22:55 +0900

    Time: 18 ms (61.11%) | Memory: 57.5 MB (61.11%) - LeetSync

---
+++ 2882-ways-to-express-an-integer-as-sum-of-powers/ways-to-express-an-integer-as-sum-of-powers.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} n
+ * @param {number} x
+ * @return {number}
+ */
+var numberOfWays = function(n, x) {
+    
+    const MOD = 10**9 + 7;
+
+    const dp = new Array(n + 1).fill(0);
+    dp[0] = 1;
+
+    for (let i = 1; i <= n; i++) {
+        const value = i ** x;
+
+        if (value > n) break;
+        for (let j = n; j >= value; j --) {
+            dp[j] = (dp[j] + dp[j - value]) % MOD;
+        }
+    }
+    return dp[n]
+};
