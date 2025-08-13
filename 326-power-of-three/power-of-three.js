58ab83abc4d4f47b3ac4b79e41242573ad3c83ba
Baek <74183179+Geunbaek@users.noreply.github.com>
2025-08-13 09:31:38 +0900

    Time: 6 ms (65.24%) | Memory: 64.6 MB (90.59%) - LeetSync

---
+++ 326-power-of-three/power-of-three.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfThree = function(n) {
+    while (n >= 3) {
+        if (n % 3 !== 0) return false;
+        n /= 3;
+    }
+    return n === 1;
+};
