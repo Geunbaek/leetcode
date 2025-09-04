60e1eb7fea7e5df783ff5f3f446e0145ee855946
Baek <74183179+Geunbaek@users.noreply.github.com>
2025-09-04 09:24:35 +0900

    Time: 0 ms (100.00%) | Memory: 55.3 MB (51.24%) - LeetSync

---
+++ 3830-find-closest-person/find-closest-person.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} x
+ * @param {number} y
+ * @param {number} z
+ * @return {number}
+ */
+var findClosest = function(x, y, z) {
+    const diff1 = Math.abs(z - x);
+    const diff2 = Math.abs(z - y);
+
+    if (diff1 === diff2) return 0
+
+    return diff1 > diff2 ? 2 : 1
+};
