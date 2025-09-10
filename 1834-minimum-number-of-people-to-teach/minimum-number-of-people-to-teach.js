d486548268599b83196c7f08366863c7edaf6589
Baek <74183179+Geunbaek@users.noreply.github.com>
2025-09-10 10:47:14 +0900

    Time: 105 ms (28.57%) | Memory: 72 MB (57.14%) - LeetSync

---
+++ 1834-minimum-number-of-people-to-teach/minimum-number-of-people-to-teach.js
@@ -0,0 +1,30 @@
+
+const hasEqualEl = (arr1, arr2) => {
+    return new Set(arr1).intersection(new Set(arr2)).size !== 0;
+}
+
+/**
+ * @param {number} n
+ * @param {number[][]} languages
+ * @param {number[][]} friendships
+ * @return {number}
+ */
+var minimumTeachings = function(n, languages, friendships) {
+    const needConnectedSet = friendships.reduce((acc, [u, v]) => {
+        if (!hasEqualEl(languages[u - 1], languages[v - 1])) {
+            return acc.union(new Set([u, v]));
+        }
+        return acc;
+    }, new Set());
+
+    const maxKnownLanguage = Array.from({length: n + 1}, () => new Set());
+    let maxKnownLanguageCount = 0;
+    for (const needConnected of needConnectedSet) {
+        for (const language of languages[needConnected - 1]) {
+            maxKnownLanguage[language].add(needConnected);
+            maxKnownLanguageCount = Math.max(maxKnownLanguageCount, maxKnownLanguage[language].size);
+        }
+    }
+
+    return needConnectedSet.size - maxKnownLanguageCount;
+};
