825bf4799dc2446beccd3039f7a0ada34cf245d2
Baek <74183179+Geunbaek@users.noreply.github.com>
2025-09-15 10:23:26 +0900

    Time: 0 ms (100.00%) | Memory: 54.6 MB (7.14%) - LeetSync

---
+++ 1102-check-if-a-number-is-majority-element-in-a-sorted-array/check-if-a-number-is-majority-element-in-a-sorted-array.js
@@ -0,0 +1,14 @@
+const equal = (a) => (b) => {
+    return a === b;
+}
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {boolean}
+ */
+var isMajorityElement = function(nums, target) {
+    return nums
+        .filter(equal(target))
+        .length > (nums.length / 2)
+};
