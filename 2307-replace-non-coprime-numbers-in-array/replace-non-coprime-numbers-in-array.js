ceb234040900976640e791475bf86c83e7e352ed
Baek <74183179+Geunbaek@users.noreply.github.com>
2025-09-16 09:59:06 +0900

    Time: 122 ms (0.00%) | Memory: 83.1 MB (40.00%) - LeetSync

--- 2307-replace-non-coprime-numbers-in-array/replace-non-coprime-numbers-in-array.js
+++ 2307-replace-non-coprime-numbers-in-array/replace-non-coprime-numbers-in-array.js
@@ -8,71 +8,20 @@ const lcm = (a, b) => a * b / gcd(a, b)
 
 const isCoprimes = (a, b) => gcd(a, b) <= 1
 
-class Node {
-    constructor(value, prev=null, next=null){
-        this.value = value;
-        this.prev = prev;
-        this.next = next;
-    }
-}
-
-class LL {
-    constructor(){
-        this.head = new Node(null);
-        this.tail = this.head;
-    }
-
-    push(value) {
-        this.tail.next = new Node(value, this.tail)
-        this.tail = this.tail.next;
-    }
-
-    remove(node) {
-        if (node.next !== null) {
-            node.next.prev = node.prev;
-        }
-        node.prev.next = node.next;
-        node.next = null;
-        node.prev = null;
-    }
-}
-
 /**
  * @param {number[]} nums
  * @return {number[]}
  */
 var replaceNonCoprimes = function(nums) {
-    const list = new LL();
+    const stack = [];
+
     for (const num of nums) {
-        list.push(num);
-    }
-
-    let node = list.head.next;
-    console.log("NODE", node.value)
-    console.log(gcd(899, 23))
-
-    while (node.next) {
-        if (node.prev.value !== null && !isCoprimes(node.value, node.prev.value)) {
-            node = node.prev;
-            node.value = lcm(node.value, node.next.value);
-            list.remove(node.next)
-            continue;
+        let n = num;
+        while (stack.length !== 0 && !isCoprimes(stack.at(-1), n)) {
+            n = lcm(stack.pop(), n);
         }
-        
-        if (isCoprimes(node.value, node.next.value)) {
-            node = node.next;
-        } else {
-            node.value = lcm(node.value, node.next.value);
-            list.remove(node.next);
-        }
-    }
 
-    node = list.head.next;
-    const answer = []
-    while (node) {
-        answer.push(node.value);
-        node = node.next;
+        stack.push(n);
     }
-
-    return answer;
+    return stack;
 };
