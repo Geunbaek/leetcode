441bc8f254231ff26cbbeb0e93b92880b50a4f59
Baek <74183179+Geunbaek@users.noreply.github.com>
2025-10-14 11:58:54 +0900

    Time: 226 ms (5.36%) | Memory: 65.7 MB (5.36%) - LeetSync

---
+++ 3612-adjacent-increasing-subarrays-detection-i/adjacent-increasing-subarrays-detection-i.js
@@ -0,0 +1,35 @@
+const isIncreased = (arr) => {
+    let prev = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        const now = arr[i];
+        if (now <= prev) {
+            return false;
+        }    
+        prev = now;
+    }
+    return true;
+}
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var hasIncreasingSubarrays = function(nums, k) {
+    const increasedRangeEndSet = new Set();
+
+    for (let i = 0; i < nums.length; i++) {
+        console.log(increasedRangeEndSet)
+        const range = nums.slice(i, i + k)
+        if (range.length < k) break; 
+
+        if (isIncreased(range)) {
+            if (increasedRangeEndSet.has(i)) {
+                return true;
+            }
+            increasedRangeEndSet.add(i + k);
+        }
+    }
+    console.log(increasedRangeEndSet)
+    return false;
+};
