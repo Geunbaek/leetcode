7db7b32c56cf745b1a882d9ca9af80349593920e
Baek <74183179+Geunbaek@users.noreply.github.com>
2025-11-04 09:46:38 +0900

    Time: 19 ms (51.35%) | Memory: 62.7 MB (75.68%) - LeetSync

--- 3610-find-x-sum-of-all-k-long-subarrays-i/find-x-sum-of-all-k-long-subarrays-i.js
+++ 3610-find-x-sum-of-all-k-long-subarrays-i/find-x-sum-of-all-k-long-subarrays-i.js
@@ -23,12 +23,11 @@ var findXSum = function(nums, k, x) {
     for (let i = 0; i < n; i++){
         if (i > n - k) break;
         const counter = count(nums.slice(i, i + k));
-        const mostCommon = Object.entries(counter).sort(compare).slice(0, x);
-        answer.push(
-            mostCommon.reduce((acc, cur) => {
-                return acc + Number(cur[0]) * cur[1];
-            }, 0)
-        )
+        const mostCommon = Object.entries(counter)
+                                    .sort(compare)
+                                    .slice(0, x)
+                                    .reduce((acc, cur) => acc + Number(cur[0]) * cur[1], 0)
+        answer.push(mostCommon)
     }
     return answer
 };
