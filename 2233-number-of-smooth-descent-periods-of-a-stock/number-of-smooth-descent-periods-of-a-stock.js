12575f79919bd2a5830364c4ca59d2e1c7435049
Baek <74183179+Geunbaek@users.noreply.github.com>
2025-12-15 20:11:04 +0900

    Time: 63 ms (5.00%) | Memory: 70.3 MB (5.00%) - LeetSync

---
+++ 2233-number-of-smooth-descent-periods-of-a-stock/number-of-smooth-descent-periods-of-a-stock.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var getDescentPeriods = function(prices) {
+    const n = prices.length;
+    const dp = Array.from({length: n}, () => 1);
+
+    for (let i = 1; i < n; i++) {
+        if (prices[i] - prices[i - 1] === -1) dp[i] += dp[i - 1];
+    }
+    console.log(dp)
+    return dp.reduce((acc, cur) => acc + cur, 0);
+};
