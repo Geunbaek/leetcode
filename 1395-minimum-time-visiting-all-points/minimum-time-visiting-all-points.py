73083fa7c5e637dc761d135f78f1eda698084053
Baek <74183179+Geunbaek@users.noreply.github.com>
2026-01-12 11:26:04 +0900

    Time: 0 ms (100.00%) | Memory: 19.3 MB (6.71%) - LeetSync

--- 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.py
+++ 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.py
@@ -1,11 +1,14 @@
 class Solution:
     def minTimeToVisitAllPoints(self, points: List[List[int]]) -> int:
-        def get_dist(p1, p2):
-            x, y = p1
-            nx, ny = p2
-            return max(abs(nx - x), abs(ny - y))
+        def get_dist(x1,y1,x2,y2):
+            return max(abs(y2-y1),abs(x2-x1))
+        ans=0
         
-        dist = 0
-        for p1, p2 in zip(points, points[1:]):
-            dist += get_dist(p1, p2)
-        return dist
+        n=len(points)
+        x1,y1=points[0]
+        for i in range(1,n):
+            x2,y2=points[i]
+            ans+=get_dist(x1,y1,x2,y2)
+            x1,y1=x2,y2
+        return ans
+        
