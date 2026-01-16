1ab0a36e845ddfec23956e6070b4995f27ac2792
Baek <74183179+Geunbaek@users.noreply.github.com>
2026-01-16 20:12:19 +0900

    Time: 1477 ms (35.97%) | Memory: 39.8 MB (79.88%) - LeetSync

---
+++ 3250-maximum-square-area-by-removing-fences-from-a-field/maximum-square-area-by-removing-fences-from-a-field.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maximizeSquareArea(self, m: int, n: int, hFences: List[int], vFences: List[int]) -> int:
+        vMemo = set()
+        MOD = 1_000_000_007
+        v = [1] + vFences + [n]
+        h = [1] + hFences + [m]
+        v.sort()
+        h.sort()
+
+        for i in range(len(v)):
+            for j in range(i + 1, len(v)):
+                vMemo.add(v[j] - v[i])
+        _max = -1
+        for i in range(len(h)):
+            for j in range(i + 1, len(h)):
+                if h[j] - h[i] in vMemo:
+                    _max = max(_max, (h[j] - h[i]) ** 2)
+        if _max == -1:
+            return _max
+        return _max % MOD
