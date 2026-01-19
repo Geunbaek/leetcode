f2b2c52cb435a28d2e3600cb92ebde05452b0c52
Baek <74183179+Geunbaek@users.noreply.github.com>
2026-01-19 15:29:09 +0900

    Time: 363 ms (34.82%) | Memory: 25.8 MB (37.05%) - LeetSync

--- 1413-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.py
+++ 1413-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.py
@@ -1,5 +1,15 @@
 class Solution:
     def maxSideLength(self, mat: List[List[int]], threshold: int) -> int:
+        def is_valid(k):
+            for y in range(1, r + 1):
+                for x in range(1, c + 1):
+                    if not (x + k < c + 1 and y + k < r + 1):
+                        continue
+                    area = dp[y + k][x + k] - dp[y + k][x - 1] - dp[y - 1][x + k] + dp[y - 1][x - 1]
+                    if area <= threshold:
+                        return True
+            return False
+
         r, c = len(mat), len(mat[0])
         dp = [[0 for _ in range(c + 1)] for _ in range(r + 1)]
 
@@ -12,15 +22,13 @@ class Solution:
             for y in range(1, r + 1):
                 dp[y][x] += dp[y - 1][x]
         
-        for y in range(1, r + 1):
-            for x in range(1, c + 1):
-                l = 0
-                while 1:
-                    if not (x + l < c + 1 and y + l < r + 1):
-                        break
-                    area = dp[y + l][x + l] - dp[y + l][x - 1] - dp[y - 1][x + l] + dp[y - 1][x - 1]
-                    if area <= threshold:
-                        answer = max(answer, l + 1)
-                    l += 1
+        left, right = 0, min(r, c) + 1
 
-        return answer
+        while left <= right:
+            mid = (left + right) // 2 
+
+            if is_valid(mid):
+                left = mid + 1
+            else:
+                right = mid - 1
+        return left
