44f97a46decf8ec3e5658010a447507d6173dedc
Baek <74183179+Geunbaek@users.noreply.github.com>
2026-02-04 17:41:53 +0900

    Time: 263 ms (98.76%) | Memory: 32 MB (98.96%) - LeetSync

--- 3956-trionic-array-ii/trionic-array-ii.py
+++ 3956-trionic-array-ii/trionic-array-ii.py
@@ -2,53 +2,57 @@ class Solution:
     def maxSumTrionic(self, nums: List[int]) -> int:
         n = len(nums)
         i = 0
-        ans = float('-inf')
-        
-        while i < n:
-            l = i  
-            i += 1
-            
-            while i < n and nums[i - 1] < nums[i]:
+        ans = -float('inf')
+
+        while i < n - 1:
+            if nums[i] <= nums[i + 1]:
                 i += 1
-            
-            if i == l + 1:
+                continue
+
+            p = i
+            q = i
+            mid_sum = nums[q]
+            while q < n - 1 and nums[q] > nums[q + 1]:
+                q += 1 
+                mid_sum += nums[q]
+
+            if p - 1 < 0 or nums[p] == nums[p - 1]:
+                i = q
+                continue
+
+            l = p - 1
+            left_sum = nums[l]
+            left_max = left_sum
+            while l - 1 >= 0 and nums[l] > nums[l - 1]:
+                l -= 1
+                left_sum += nums[l]
+                left_max = max(left_max, left_sum)
+
+            if l == p:
+                i = q
+                continue
+
+            if q + 1 >= n or nums[q] == nums[q + 1]:
+                i = q
+                continue
+
+            r = q + 1
+            right_sum = nums[r]
+            right_max = right_sum
+            while r + 1 < n and nums[r] < nums[r + 1]:
+                r += 1
+                right_sum += nums[r]
+                right_max = max(right_max, right_sum)
+
+            if r == q:
+                i = q
                 continue
             
-            p = i - 1  
-            
-            s = nums[p - 1] + nums[p]
-            
-            while i < n and nums[i - 1] > nums[i]:
-                s += nums[i]
-                i += 1
-            
-            if i == p + 1 or i == n or nums[i - 1] == nums[i]:
-                continue
-            
-            q = i - 1  
-            
-            s += nums[i]  
-            i += 1
-            
-            mx = 0
-            t = 0
-            while i < n and nums[i - 1] < nums[i]:
-                t += nums[i]
-                mx = max(mx, t)
-                i += 1
-            s += mx
-            
-            mx = 0
-            t = 0
-            j = p - 2
-            while j >= l:
-                t += nums[j]
-                mx = max(mx, t)
-                j -= 1
-            s += mx
-            
-            ans = max(ans, s)
-            
+            _sum = left_max + mid_sum + right_max
+            print(left_max, mid_sum, right_max)
+            print(l, p, q, r)
+            print(_sum)
+            ans = max(ans, _sum)
             i = q
-        
+
         return ans
