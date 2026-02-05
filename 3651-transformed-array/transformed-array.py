3b7f8d8a94b7cc3bdd8cad7394864bedaa8b5807
Baek <74183179+Geunbaek@users.noreply.github.com>
2026-02-05 09:21:53 +0900

    Time: 42 ms (99.10%) | Memory: 19.5 MB (10.76%) - LeetSync

--- 3651-transformed-array/transformed-array.py
+++ 3651-transformed-array/transformed-array.py
@@ -3,12 +3,5 @@ class Solution:
         n = len(nums)
         result = []
         for i, num in enumerate(nums):
-            if num > 0:
-                result.append(nums[(i + num) % n])
-            else:
-                target = (i - abs(num)) % n
-                if target < 0:
-                    result.append(nums[n - target])
-                else:
-                    result.append(nums[target])
+            result.append(nums[(i + num) % n])
         return result 
