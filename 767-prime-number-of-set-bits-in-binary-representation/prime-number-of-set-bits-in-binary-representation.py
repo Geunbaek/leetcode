2278db61dba018ba88670d2540d41f5a564a93ca
Baek <74183179+Geunbaek@users.noreply.github.com>
2026-02-21 10:08:25 +0900

    Time: 64 ms (68.45%) | Memory: 19.4 MB (40.29%) - LeetSync

--- 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.py
+++ 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.py
@@ -3,16 +3,14 @@ class Solution:
         def count_bits(num):
             return bin(num).count('1')
         
-        def is_prime(num):
-            if num <= 1:
-                return False
-
-            for i in range(2, int(math.sqrt(num)) + 1):
-                if num % i == 0:
-                    return False
-            return True
-    
+        dp = [0 for _ in range(20)]
+        dp[0], dp[1] = 1, 1
+        for i in range(2, 20):
+            if dp[i] == 1:
+                continue
+            for j in range(2 * i, 20, i):
+                dp[j] = 1
         answer = 0
         for num in range(left, right + 1):
-            answer += 1 if is_prime(count_bits(num)) else 0
+            answer += 0 if dp[count_bits(num)] else 1
         return answer
