643fb0833dcd46a45ba2de2f2712e89a78b7d269
Baek <74183179+Geunbaek@users.noreply.github.com>
2026-03-10 12:26:51 +0900

    Time: 4733 ms (25.58%) | Memory: 169.2 MB (62.79%) - LeetSync

--- 3407-find-all-possible-stable-binary-arrays-ii/find-all-possible-stable-binary-arrays-ii.py
+++ 3407-find-all-possible-stable-binary-arrays-ii/find-all-possible-stable-binary-arrays-ii.py
@@ -1,27 +1,28 @@
 class Solution:
     def numberOfStableArrays(self, zero: int, one: int, limit: int) -> int:
-        MOD = 10**9 + 7
-        # dp[i][j][last] 형태
+        mod = 10**9 + 7
+
         dp = [[[0, 0] for _ in range(one + 1)] for _ in range(zero + 1)]
-        
-        # 초기값 설정: 0만 쓰거나 1만 쓰는 경우 (limit 이하일 때만 1가지)
-        for i in range(min(zero, limit) + 1):
-            dp[i][0][0] = 1
-        for j in range(min(one, limit) + 1):
-            dp[0][j][1] = 1
-            
-        for i in range(1, zero + 1):
-            for j in range(1, one + 1):
-                # 0을 추가하는 경우
-                dp[i][j][0] = (dp[i-1][j][0] + dp[i-1][j][1]) % MOD
-                if i > limit:
-                    # 연속해서 0이 limit+1개 나오는 경우를 제외
-                    dp[i][j][0] = (dp[i][j][0] - dp[i-limit-1][j][1] + MOD) % MOD
-                
-                # 1을 추가하는 경우
-                dp[i][j][1] = (dp[i][j-1][0] + dp[i][j-1][1]) % MOD
-                if j > limit:
-                    # 연속해서 1이 limit+1개 나오는 경우를 제외
-                    dp[i][j][1] = (dp[i][j][1] - dp[i][j-limit-1][0] + MOD) % MOD
-                    
-        return (dp[zero][one][0] + dp[zero][one][1]) % MOD
+        for i in range(zero + 1):
+            for j in range(one + 1):
+                for lastBit in range(2):
+                    if i == 0:
+                        if lastBit == 0 or j > limit:
+                            dp[i][j][lastBit] = 0
+                        else:
+                            dp[i][j][lastBit] = 1
+                    elif j == 0:
+                        if lastBit == 1 or i > limit:
+                            dp[i][j][lastBit] = 0
+                        else:
+                            dp[i][j][lastBit] = 1
+                    elif lastBit == 0:
+                        dp[i][j][lastBit] = dp[i - 1][j][0] + dp[i - 1][j][1]
+                        if i > limit:
+                            dp[i][j][lastBit] -= dp[i - limit - 1][j][1]
+                    else:
+                        dp[i][j][lastBit] = dp[i][j - 1][0] + dp[i][j - 1][1]
+                        if j > limit:
+                            dp[i][j][lastBit] -= dp[i][j - limit - 1][0]
+                    dp[i][j][lastBit] %= mod
+        return (dp[-1][-1][0] + dp[-1][-1][1]) % mod
