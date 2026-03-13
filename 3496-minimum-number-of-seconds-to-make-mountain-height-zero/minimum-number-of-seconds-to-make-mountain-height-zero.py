530a2029b18f7e57414a08373b13ef7b6cfbe80f
Baek <74183179+Geunbaek@users.noreply.github.com>
2026-03-13 22:34:45 +0900

    Time: 547 ms (22.15%) | Memory: 20.5 MB (96.84%) - LeetSync

---
+++ 3496-minimum-number-of-seconds-to-make-mountain-height-zero/minimum-number-of-seconds-to-make-mountain-height-zero.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minNumberOfSeconds(self, mountainHeight: int, workerTimes: List[int]) -> int:
+        maxWorkerTimes = max(workerTimes)
+        l, r = 1, maxWorkerTimes * mountainHeight * (mountainHeight + 1) // 2
+        eps = 1e-7
+        answer = 0
+        while l <= r:
+            time = (l + r) // 2
+            heights = 0
+            
+            for workerTime in workerTimes:
+                work = time // workerTime
+                k = int((-1 + ((1 + work * 8) ** 0.5)) / 2 + eps)
+                heights += k
+            
+            if heights >= mountainHeight:
+                answer = time
+                r = time - 1
+            else:
+                l = time + 1
+        return answer
