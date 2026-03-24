f1fce2c28fbd4951328ffbbac78fc98dc9222156
Baek <74183179+Geunbaek@users.noreply.github.com>
2026-03-24 17:53:45 +0900

    Time: 110 ms (71.52%) | Memory: 44.5 MB (47.02%) - LeetSync

--- 3031-construct-product-matrix/construct-product-matrix.py
+++ 3031-construct-product-matrix/construct-product-matrix.py
@@ -4,16 +4,16 @@ class Solution:
         r, c = len(grid), len(grid[0])
         p = [[0 for _ in range(c)] for _ in range(r)]
 
-        suffix = 1
-        for y in range(r - 1, -1, -1):
-            for x in range(c - 1, -1, -1):
-                p[y][x] = suffix
-                suffix = (suffix * grid[y][x]) % MOD
-
         prefix = 1
         for y in range(r):
             for x in range(c):
-                p[y][x] = (p[y][x] * prefix) % MOD
+                p[y][x] = prefix
                 prefix = (prefix * grid[y][x]) % MOD
+
+        suffix = 1
+        for y in range(r - 1, -1, -1):
+            for x in range(c - 1, -1, -1):
+                p[y][x] = p[y][x] * suffix % MOD
+                suffix = (suffix * grid[y][x]) % MOD
 
         return p
