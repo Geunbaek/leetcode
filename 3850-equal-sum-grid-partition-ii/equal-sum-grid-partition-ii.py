f231c5ba17174b954c782cdda991d6fca0f2df65
Baek <74183179+Geunbaek@users.noreply.github.com>
2026-03-26 11:56:05 +0900

    Time: 1275 ms (36.11%) | Memory: 47.4 MB (97.22%) - LeetSync

--- 3850-equal-sum-grid-partition-ii/equal-sum-grid-partition-ii.py
+++ 3850-equal-sum-grid-partition-ii/equal-sum-grid-partition-ii.py
@@ -1,93 +1,57 @@
+from collections import defaultdict
+from typing import List
+
 class Solution:
     def canPartitionGrid(self, grid: List[List[int]]) -> bool:
-        r, c = len(grid), len(grid[0])
-
-        top = defaultdict(int)
-        top_total = 0
-
-        bottom = defaultdict(int)
-        bottom_total = 0
-
-        for y in range(r):
-            for x in range(c):
-                bottom[grid[y][x]] += 1
-                bottom_total += grid[y][x]
-
-        for y in range(r - 1):
-            for x in range(c):
-                bottom[grid[y][x]] -= 1
-                bottom_total -= grid[y][x]
-                top[grid[y][x]] += 1
-                top_total += grid[y][x]
-
-            if bottom_total == top_total:
-                return True
+        def check_split(g: List[List[int]]) -> bool:
+            r, c = len(g), len(g[0])
             
-            diff = abs(top_total - bottom_total)
-            top_len = y + 1
-            bottom_len = r - top_len
-            if top_total > bottom_total and top_len == 1 and diff in [grid[y][0], grid[y][-1]]:
-                return True
-            if top_total < bottom_total and bottom_len == 1 and diff in [grid[r - 1][0], grid[r - 1][-1]]:
-                return True
-            if top_total > bottom_total and top_len != 1:
-                if c == 1:
-                    if diff in [grid[0][0], grid[y][0]]:
-                        return True
-                else:    
-                    if top[diff] != 0:
-                        return True
-            if top_total < bottom_total and bottom_len != 1:
-                if c == 1:
-                    if diff in [grid[y + 1][0], grid[-1][0]]:
-                        return True
-                else:    
-                    if bottom[diff] != 0:
-                        return True
-        
-        left = defaultdict(int)
-        left_total = 0
-
-        right = defaultdict(int)
-        right_total = 0
-
-        for y in range(r):
-            for x in range(c):
-                right[grid[y][x]] += 1
-                right_total += grid[y][x]
-
-        for x in range(c - 1):
-            for y in range(r):
-                right[grid[y][x]] -= 1
-                right_total -= grid[y][x]
-                left[grid[y][x]] += 1
-                left_total += grid[y][x]
-
-            if right_total == left_total:
-                return True
-            diff = abs(right_total - left_total)
-            left_len = x + 1
-            right_len = c - left_len
-            if right_total < left_total and left_len == 1 and diff in [grid[0][x], grid[-1][x]]:
-                return True
-            if right_total > left_total and right_len == 1 and diff in [grid[0][c - 1], grid[-1][c - 1]]:
-                return True
-            if right_total < left_total and left_len != 1:
-                if r == 1:
-                    if diff in [grid[0][0], grid[0][x]]:
-                        return True
-                else:    
-                    if left[diff] != 0:
-                        return True
-            if right_total > left_total and right_len != 1:
-                if r == 1:
-                    if diff in [grid[0][x + 1], grid[0][-1]]:
-                        return True
-                else:    
-                    if right[diff] != 0:
-                        return True
-
-
-        return False
-        
+            top = defaultdict(int)
+            bottom = defaultdict(int)
             
+            bottom_total = 0
+            for row in g:
+                for val in row:
+                    bottom[val] += 1
+                    bottom_total += val
+
+            top_total = 0
+
+            for y in range(r - 1):
+                for val in g[y]:
+                    bottom[val] -= 1
+                    bottom_total -= val
+                    top[val] += 1
+                    top_total += val
+
+                if bottom_total == top_total:
+                    return True
+                
+                diff = abs(top_total - bottom_total)
+                top_len = y + 1
+                bottom_len = r - top_len
+                
+                if top_total > bottom_total:
+                    if top_len == 1:
+                        if diff in (g[y][0], g[y][-1]): return True
+                    elif c == 1:
+                        if diff in (g[0][0], g[y][0]): return True
+                    elif top.get(diff, 0) > 0: 
+                        return True
+                        
+                elif top_total < bottom_total:
+                    if bottom_len == 1:
+                        if diff in (g[-1][0], g[-1][-1]): return True
+                    elif c == 1:
+                        if diff in (g[y + 1][0], g[-1][0]): return True
+                    elif bottom.get(diff, 0) > 0:
+                        return True
+                        
+            return False
+
+        if check_split(grid):
+            return True
+            
+        transposed_grid = [list(col) for col in zip(*grid)]
+        
+        return check_split(transposed_grid)
