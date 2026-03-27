54fe39b3d0377c0e19b9af42e1a8daeba2cf2692
Baek <74183179+Geunbaek@users.noreply.github.com>
2026-03-27 13:23:26 +0900

    Time: 11 ms (13.53%) | Memory: 19.5 MB (10.59%) - LeetSync

--- 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.py
+++ 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.py
@@ -1,19 +1,13 @@
 class Solution:
     def areSimilar(self, mat: List[List[int]], k: int) -> bool:
         r, c = len(mat), len(mat[0])
-        temp = [row[::] for row in mat]
+        temp = [deque(row[::]) for row in mat]
         k %= c
         def shift_left(y):
-            first = temp[y][0]
-            for x in range(c - 1):
-                temp[y][x] = temp[y][x + 1]
-            temp[y][-1] = first
+            temp[y].rotate()
 
         def shift_right(y):
-            last = temp[y][-1]
-            for x in range(c - 1, 0, -1):
-                temp[y][x] = temp[y][x - 1]
-            temp[y][0] = last
+            temp[y].rotate(-1)
 
         def _shift():
             for y in range(r):
