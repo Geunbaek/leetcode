5b9c18b9240e06fbb6b221e86f93fa1d07730bd3
Baek <74183179+Geunbaek@users.noreply.github.com>
2026-04-01 11:03:47 +0900

    Time: 136 ms (60.12%) | Memory: 51.4 MB (35.71%) - LeetSync

--- 2846-robot-collisions/robot-collisions.py
+++ 2846-robot-collisions/robot-collisions.py
@@ -1,31 +1,38 @@
 class Solution:
     def survivedRobotsHealths(self, positions: List[int], healths: List[int], directions: str) -> List[int]:
-        status = []
-
-        for i, (p, h, d) in enumerate(zip(positions, healths, directions)):
-            status.append([i, p, h, d])
-        status.sort(key = lambda x: x[1])
-
         stack = []
+        l_stack = []
+        
+        robots = []
+        
+        for i, (p, h, d) in enumerate(zip(positions, healths, directions)):
+            robots.append((p, h, d, i))
+        robots.sort()
+        
+        for p, h, d, i in robots:
+            if d == "R":
+                stack.append((p, h, d, i))
+            else:
+                if not stack:
+                    l_stack.append((p, h, d, i))
+                    continue
+                
+                while stack:
+                    p1, h1, d1, i1 = stack.pop()
+                    if h1 < h:
+                        h -= 1
+                    elif h1 > h:
+                        h = 0
+                        stack.append((p1, h1 - 1, d1, i1))
+                        break
+                    else:
+                        h = 0
+                        break
 
-        for i, (j, p, h, d) in enumerate(status):
-            while stack and d == 'L' and stack[-1][-1] == 'R' and status[i][2] > 0:
-                last_health = status[stack[-1][0]][2]
-                now_health = status[i][2]
-                if last_health == now_health:
-                    status[stack[-1][0]][2] = 0
-                    status[i][2] = 0
-                    stack.pop()
-                    break
-                elif last_health > now_health:
-                    status[stack[-1][0]][2] -= 1
-                    status[i][2] = 0
-                    break
-                else:
-                    status[i][2] -= 1
-                    status[stack[-1][0]][2] = 0
-                    stack.pop()
-            
-            if status[i][2] > 0:
-                stack.append((i, d))
-        return list(map(lambda x: x[2], sorted(map(lambda x: status[x[0]], stack), key = lambda x: x[0])))
+                if not stack and h:
+                    l_stack.append((p, h, d, i))
+        stack.extend(l_stack)
+        stack.sort(key = lambda x: x[-1])
+        answer = list(map(lambda x: x[1], stack))
+        return answer
+        
