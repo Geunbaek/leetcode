b72bec7d4dc9dda1bceb00c462f05924c4519977
Baek <74183179+Geunbaek@users.noreply.github.com>
2026-04-05 11:12:33 +0900

    Time: 11 ms (75.07%) | Memory: 19.3 MB (67.40%) - LeetSync

--- 657-robot-return-to-origin/robot-return-to-origin.py
+++ 657-robot-return-to-origin/robot-return-to-origin.py
@@ -1,8 +1,4 @@
 class Solution:
     def judgeCircle(self, moves: str) -> bool:
-        m = defaultdict(int)
-
-        for move in moves:
-            m[move] += 1
-
+        m = Counter(moves)
         return m['D'] == m['U'] and m['L'] == m['R']
