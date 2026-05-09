fca3db28ecd1136349f0f5b3671b55451fcbec55
Baek <74183179+Geunbaek@users.noreply.github.com>
2026-05-09 21:10:56 +0900

    Time: 692 ms (5.71%) | Memory: 19.6 MB (85.71%) - LeetSync

--- 2043-cyclically-rotating-a-grid/cyclically-rotating-a-grid.py
+++ 2043-cyclically-rotating-a-grid/cyclically-rotating-a-grid.py
@@ -1,6 +1,6 @@
 class Solution:
     def rotateGrid(self, grid: List[List[int]], k: int) -> List[List[int]]:
-        def rotate(sx, sy, repeat, is_last):
+        def rotate(sx, sy, is_visit, is_mut):
             q = deque([(sx, sy, 0, grid[sy][sx])])
             while q:
                 x, y, d, value = q.popleft()
@@ -15,28 +15,12 @@ class Solution:
                     q.append((x, y, d + 1, value))
                     continue
                 prev = grid[ny][nx]
-                if is_last:
+                if is_visit:
                     visited[ny][nx] = 1
-                grid[ny][nx] = value
+                if is_mut:
+                    grid[ny][nx] = value
                 q.append((nx, ny, d, prev))
 
-        def maintain(sx, sy):
-            q = deque([(sx, sy, 0)])
-            while q:
-                x, y, d = q.popleft()
-                if d == 3 and sx == x and y == sy:
-                    return
-                nx = x + dx[d]
-                ny = y + dy[d]
-                if not (0 <= nx < n and 0 <= ny < m):
-                    q.append((x, y, d + 1))
-                    continue
-                if visited[ny][nx] != 0:
-                    q.append((x, y, d + 1))
-                    continue
-                visited[ny][nx] = 1
-                q.append((nx, ny, d))
-                
         def calc_circum(depth):
             w = (n - (depth * 2) - 2) * 2
             h = (m - (depth * 2) - 2) * 2
@@ -51,7 +35,7 @@ class Solution:
                 break
             count = k % calc_circum(sx)
             for repeat in range(count):
-                rotate(sx, sy, repeat + 1, repeat == count - 1)
+                rotate(sx, sy, repeat == count - 1, 1)
             if count == 0:
-                maintain(sx, sy)
+                rotate(sx, sy, 1, 0)
         return grid
