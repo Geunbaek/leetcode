452b9f4dcd089867038cafc92023701957114f46
Baek <74183179+Geunbaek@users.noreply.github.com>
2026-05-16 16:18:55 +0900

    Time: 0 ms (100.00%) | Memory: 2.1 MB (80.95%) - LeetSync

--- 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.rs
+++ 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.rs
@@ -1,9 +1,5 @@
 impl Solution {
     pub fn find_min(nums: Vec<i32>) -> i32 {
-        let min_value = nums.iter().min();
-        match min_value {
-            Some(min) => return *min,
-            None => return -1,
-        }
+        *nums.iter().min().unwrap()
     }
 }
