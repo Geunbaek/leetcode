4a40fa10545cf92fca93116769a5bb6ec155a14d
Baek <74183179+Geunbaek@users.noreply.github.com>
2026-05-19 10:44:03 +0900

    Time: 28 ms (7.25%) | Memory: 45.8 MB (8.49%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.py
+++ 2634-minimum-common-value/minimum-common-value.py
@@ -1,9 +1,9 @@
 class Solution:
     def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
-        num2_set = set(nums2)
+        nums2_set = set(nums2)
 
-        nums1.sort()
+        nums1 = sorted(list(set(nums1)))
         for num in nums1:
-            if num in num2_set:
+            if num in nums2_set:
                 return num
         return -1
