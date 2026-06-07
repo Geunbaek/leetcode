850a9d74c5f4f5c94e7e6e05bfb82352f2655ab4
Baek <74183179+Geunbaek@users.noreply.github.com>
2026-06-07 09:46:47 +0900

    Time: 303 ms (5.28%) | Memory: 34.3 MB (9.35%) - LeetSync

---
+++ 2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.py
@@ -0,0 +1,42 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def createBinaryTree(self, descriptions: List[List[int]]) -> Optional[TreeNode]:
+        p = defaultdict(int)
+        cache = {}
+
+        def find(x):
+            if p[x] != x:
+                p[x] = find(p[x])
+            return p[x]
+        
+        def union(a, b):
+            ap = find(a)
+            bp = find(b)
+            p[ap] = bp
+
+        for parent, child, is_left in descriptions:
+            if parent not in cache:
+                cache[parent] = TreeNode(parent)
+                p[parent] = parent
+            if child not in cache:
+                cache[child] = TreeNode(child)
+                p[child] = child
+            if is_left:
+                cache[parent].left = cache[child]
+            else:
+                cache[parent].right = cache[child]
+            
+            union(child, parent)
+        root = None
+
+        for key in p.keys():
+            root = find(key)
+        
+        return cache[root]
+
+
