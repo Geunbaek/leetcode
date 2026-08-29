6cad660e3a1e00171a86de16d35702b3499a6685
Baek <74183179+Geunbaek@users.noreply.github.com>
2026-08-29 15:24:55 +0900

    Time: 833 ms (5.22%) | Memory: 45.9 MB (88.06%) - LeetSync

--- 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.py
+++ 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.py
@@ -1,27 +1,22 @@
 class Solution:
-    def lexicographicallySmallestArray(self, nums, limit):
-        nums_sorted = sorted(nums)
-
-        curr_group = 0
-        num_to_group = {}
-        num_to_group[nums_sorted[0]] = curr_group
-
-        group_to_list = {}
-        group_to_list[curr_group] = deque([nums_sorted[0]])
-
-        for i in range(1, len(nums)):
-            if abs(nums_sorted[i] - nums_sorted[i - 1]) > limit:
-                curr_group += 1
-
-            num_to_group[nums_sorted[i]] = curr_group
-
-            if curr_group not in group_to_list:
-                group_to_list[curr_group] = deque()
-            group_to_list[curr_group].append(nums_sorted[i])
-
-        for i in range(len(nums)):
-            num = nums[i]
-            group = num_to_group[num]
-            nums[i] = group_to_list[group].popleft()
-
-        return nums
+    def lexicographicallySmallestArray(self, nums: List[int], limit: int) -> List[int]:
+        res = [0 for _ in range(len(nums))]
+        h = []
+        for i, num in enumerate(nums):
+            heapq.heappush(h, (num, i))
+        
+        while h:
+            num, index = heapq.heappop(h)
+            index_h = [index]
+            num_h = [num]
+            while h and h[0][0] - num <= limit:
+                num, index = heapq.heappop(h)
+                heapq.heappush(index_h, index)
+                heapq.heappush(num_h, num)
+                
+            while index_h:
+                index = heapq.heappop(index_h)
+                num = heapq.heappop(num_h)
+                res[index] = num
+        
+        return res
